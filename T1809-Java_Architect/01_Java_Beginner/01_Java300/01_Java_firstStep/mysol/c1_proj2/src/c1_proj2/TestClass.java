07a3b62f3a6f561fe27d2d92f0c2fb4eee0f1884
specter01wj <specter01wj@gmail.com>
2018-11-13 00:23:27 -0500

    11/12/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c1_proj2/src/c1_proj2/TestClass.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c1_proj2/src/c1_proj2/TestClass.java
@@ -2,11 +2,18 @@ package c1_proj2;
 
 public class TestClass {
 
+	int a;
+	
 	public static void main(String[] args) {
 		//char
 		int $123 = 1;
 		//variable
-		
+		int age = 19;
+		//var classify
+		{
+			int i=11;
+		}
+		//System.out.println(i);
 		
 		
 		
