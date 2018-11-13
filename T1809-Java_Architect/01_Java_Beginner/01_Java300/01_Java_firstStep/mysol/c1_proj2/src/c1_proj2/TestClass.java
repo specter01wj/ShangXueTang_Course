1f0970c425f781c8aa53a0a594db003358d52938
specter01wj <specter01wj@gmail.com>
2018-11-13 01:14:34 -0500

    11/12/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c1_proj2/src/c1_proj2/TestClass.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c1_proj2/src/c1_proj2/TestClass.java
@@ -1,5 +1,7 @@
 package c1_proj2;
 
+import java.math.*;
+
 public class TestClass {
 
 	int a;
@@ -22,6 +24,25 @@ public class TestClass {
 		//--integer
 		int b = 015, c = 0x15, d = 0b1101;
 		System.out.println(b + " ," + c + " ," + d);
+		//--float
+		float a1 = 3.14f;
+		double b1 = 628e-2;
+		System.out.println(a1==b1/2);
+		
+		System.out.println("##################");
+		BigDecimal bd = BigDecimal.valueOf(1.0);
+		bd = bd.subtract(BigDecimal.valueOf(0.1));
+		bd = bd.subtract(BigDecimal.valueOf(0.1));
+		bd = bd.subtract(BigDecimal.valueOf(0.1));
+		bd = bd.subtract(BigDecimal.valueOf(0.1));
+		bd = bd.subtract(BigDecimal.valueOf(0.1));
+		System.out.println(bd);// 0.5
+		System.out.println(1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1);// 0.5000000000000001
+		
+		BigDecimal  bd2 = BigDecimal.valueOf(0.1);
+		BigDecimal  bd3 = BigDecimal.valueOf(1.0/10.0);
+		
+		System.out.println(bd2.equals(bd3)); 
 	}
 	
 }
