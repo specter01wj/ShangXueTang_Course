0a9dd3c2c8800526ee22667ceea5fa8974939889
specter01wj <specter01wj@gmail.com>
2018-12-02 15:15:06 -0500

    11/12/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c1_proj2/src/c1_proj2/TestClass.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c1_proj2/src/c1_proj2/TestClass.java
@@ -196,6 +196,16 @@ public class TestClass {
 		}
 		System.out.println("Sum:(for) " + sum08);
 		
+		// 054
+		for(int i=1; i <= 5; i++) {
+			for(int j=1; j <= 5; j++) {
+				System.out.print(i + "  ");
+			}
+			System.out.println();
+		}
+		
+		
+		
 		
 		
 	}
