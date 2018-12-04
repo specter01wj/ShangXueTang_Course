82e0ca02073c435cfe94113fad9932e6d2d1674c
specter01wj <specter01wj@gmail.com>
2018-12-04 18:32:39 -0500

    11/23/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj1/src/c2_proj1/TestClass2.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj1/src/c2_proj1/TestClass2.java
@@ -2,8 +2,22 @@ package c2_proj1;
 
 public class TestClass2 {
 
+	int id, age;
+	String name;
+	
+	void study() {
+		System.out.println("Studying!");
+	}
+	
+	void play() {
+		System.out.println("Playing!");
+	}
+	
 	public static void main(String[] args) {
 		
+		// 062
+		TestClass2 tl2 = new TestClass2();
+		tl2.play();
 		
 		
 	}
