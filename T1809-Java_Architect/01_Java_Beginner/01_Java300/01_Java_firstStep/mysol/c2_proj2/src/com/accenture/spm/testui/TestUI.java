dfa8fe0b742c96750d95e96d29028e5cc0e7db4d
specter01wj <specter01wj@gmail.com>
2018-12-13 01:42:39 -0500

    12/13/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/testui/TestUI.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/testui/TestUI.java
@@ -2,12 +2,19 @@ package com.accenture.spm.testui;
 
 import com.accenture.spm.oo.*;
 
+import static java.lang.Math.*;
+
 public class TestUI {
 	
 	public static void main(String[] args) {
 		
 		// 073
 		User01 user01 = new User01();
+		
+		System.out.println(PI);
+		
+		
+		
 		
 		
 	}
