62cbe27b433c536aae11a9a358f6538c94fd6b30
specter01wj <specter01wj@gmail.com>
2018-12-14 18:02:59 -0500

    11/23/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo2/Student1.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo2/Student1.java
@@ -7,4 +7,10 @@ public class Student1 extends Person1 {
 		System.out.println("Study for 2 hours!");
 	}
 	
+	public Student1(String name, int height, String major) {
+		this.name = name;
+		this.height = height;
+		this.major = major;
+	}
+	
 }
