1cbedc35f57df2c6cb95ea84b6a4749ec8551348
specter01wj <specter01wj@gmail.com>
2018-12-16 17:43:22 -0500

    11/23/2018 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo77/User77.java
@@ -0,0 +1,17 @@
+package com.accenture.spm.oo77;
+
+public class User77 {
+
+	int id;
+	String name, pwd;
+	
+	public User77(int id, String name, String pwd) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.pwd = pwd;
+	}
+	
+	
+	
+}
