532c4bd37518d868c840b28833b074ad3a859c4b
specter01wj <specter01wj@gmail.com>
2018-12-15 19:36:07 -0500

    11/23/2018 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo75/Horse75.java
@@ -0,0 +1,8 @@
+package com.accenture.spm.oo75;
+
+public class Horse75 extends Vehicle75 {
+
+	
+	
+	
+}
--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo75/Vehicle75.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo75/Vehicle75.java
@@ -2,6 +2,12 @@ package com.accenture.spm.oo75;
 
 public class Vehicle75 {
 
+	public void run() {
+		System.out.println("Run!");
+	}
 	
+	public void stop() {
+		System.out.println("Stop!");
+	}
 	
 }
