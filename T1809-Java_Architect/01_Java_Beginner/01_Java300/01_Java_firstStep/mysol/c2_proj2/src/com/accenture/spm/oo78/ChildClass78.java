8abd4a78630d35f7d71709bf120b87835095738d
specter01wj <specter01wj@gmail.com>
2018-12-16 23:54:59 -0500

    12/16/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo78/ChildClass78.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo78/ChildClass78.java
@@ -1,10 +1,18 @@
 package com.accenture.spm.oo78;
 
-public class ChildClass78 {
+import com.accenture.spm.oo78.*;
+
+public class ChildClass78 extends FatherClass78 {
 
 	public int value;
 	
-	
+	public void f() {
+		super.f();
+		value = 200;
+		System.out.println("ChildClass value = " + value);
+		System.out.println(value);
+		System.out.println(super.value);
+	}
 	
 	
 }
