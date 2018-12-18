a249abff134cbabd44cd54edfc0ec4916171516b
specter01wj <specter01wj@gmail.com>
2018-12-18 16:00:22 -0500

    12/16/2018 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo81/Animal81.java
@@ -0,0 +1,9 @@
+package com.accenture.spm.oo81;
+
+public class Animal81 {
+
+	public void shout() {
+		System.out.println("Shout!");
+	}
+	
+}
---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c2_proj2/src/com/accenture/spm/oo81/Cat81.java
@@ -0,0 +1,5 @@
+package com.accenture.spm.oo81;
+
+public class Cat81 {
+
+}
