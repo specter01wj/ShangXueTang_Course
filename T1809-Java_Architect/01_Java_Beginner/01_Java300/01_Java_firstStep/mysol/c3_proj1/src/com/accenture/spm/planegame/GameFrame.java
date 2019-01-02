63ed394fe8e763ea9743dd5d4962b66693d416d9
specter01wj <specter01wj@gmail.com>
2019-01-01 20:19:53 -0500

    12/16/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/GameFrame.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/GameFrame.java
@@ -4,6 +4,16 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 
+	public void launchFrame() {
+		this.setTitle("PlaneGame by James");
+		this.setVisible(true);
+	}
 	
+	public static void main(String[] args) {
+		
+		GameFrame gf = new GameFrame();
+		gf.launchFrame();
+		
+	}
 	
 }
