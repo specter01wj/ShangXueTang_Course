47bf6a467cf05666b1a2bb3d5763e17d4f4361e8
specter01wj <specter01wj@gmail.com>
2019-01-02 03:10:14 -0500

    12/16/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/GameFrame.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/GameFrame.java
@@ -2,12 +2,17 @@ package com.accenture.spm.planegame;
 
 import javax.swing.JFrame;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class GameFrame extends JFrame {
+	
+	Image ball = GameUtil.getImage("images/ball.png");
 
 	public void launchFrame() {
 		this.setTitle("PlaneGame by James");
@@ -25,12 +30,22 @@ public class GameFrame extends JFrame {
 	
 	@Override
 	public void paint(Graphics g) {
+		Color c = g.getColor();
+		Font f = g.getFont();
+		g.setColor(Color.CYAN);
+		
 		g.drawLine(100, 100, 300, 300);
 		g.drawRect(100, 100, 300, 300);
 		g.drawOval(100, 100, 300, 300);
 		g.fillRect(100,100, 20, 20);
+		g.setColor(Color.red);
+		g.setFont(new Font("Times New Roman", Font.BOLD, 50));
+		g.drawString("James", 200, 200);
 		
+		g.drawImage(ball, 50, 50, null);
 		
+		g.setColor(c);
+		g.setFont(f);
 	}
 	
 	
