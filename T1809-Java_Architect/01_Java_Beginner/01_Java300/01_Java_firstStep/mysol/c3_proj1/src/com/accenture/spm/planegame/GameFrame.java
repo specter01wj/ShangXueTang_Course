478510095c0505845c260789551eb8e6bcbdda6c
specter01wj <specter01wj@gmail.com>
2019-01-01 20:31:11 -0500

    12/16/2018 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/GameFrame.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/GameFrame.java
@@ -1,12 +1,24 @@
 package com.accenture.spm.planegame;
 
 import javax.swing.JFrame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameFrame extends JFrame {
 
 	public void launchFrame() {
 		this.setTitle("PlaneGame by James");
+		this.setSize(500, 500);
+		this.setLocation(300, 300);
 		this.setVisible(true);
+		
+		this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 	}
 	
 	public static void main(String[] args) {
