1b4b6ac9c458e99e2aba08aa48597d11a60ef99b
specter01wj <specter01wj@gmail.com>
2019-01-06 01:21:51 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/Plane.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/01_Java_firstStep/mysol/c3_proj1/src/com/accenture/spm/planegame/Plane.java
@@ -2,9 +2,12 @@ package com.accenture.spm.planegame;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
 
 public class Plane extends GameObject {
 
+	boolean left, right, up, down;
+	
 	public void drawSelf(Graphics g) {
 		g.drawImage(img, (int)x, (int)y, null);
 		x++;
@@ -14,6 +17,10 @@ public class Plane extends GameObject {
 		this.img = img;
 		this.x = x;
 		this.y = y;
+	}
+	
+	public void addDirection(KeyEvent e) {
+		
 	}
 	
 }
