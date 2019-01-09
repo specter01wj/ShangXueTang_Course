f7e7fdaf7a714a4df14921a58ceac76e20469910
specter01wj <specter01wj@gmail.com>
2019-01-09 01:13:21 -0500

    01/05/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestInnerClass.java
@@ -0,0 +1,19 @@
+package com.accenture.spm.oop;
+
+public class TestInnerClass {
+
+}
+
+class Outer {
+	
+	private int age = 23;
+	
+	public void testOuter() {
+		System.out.println("Test Outer!");
+	}
+	
+	class Inner {
+		
+	}
+	
+}
