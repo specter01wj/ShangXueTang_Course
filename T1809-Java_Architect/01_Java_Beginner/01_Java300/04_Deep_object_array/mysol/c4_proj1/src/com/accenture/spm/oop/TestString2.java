b11d01c78e8c0a614ed70f7fc1c4b0932296907b
specter01wj <specter01wj@gmail.com>
2019-01-10 01:59:37 -0500

    01/05/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestString2.java
@@ -0,0 +1,13 @@
+package com.accenture.spm.oop;
+
+public class TestString2 {
+
+	public static void main(String[] args) {
+		
+		String s1 = "core Java", s2 = "Core Java";
+		
+		System.out.println(s1.charAt(3));
+		System.out.println(s2.length());
+	}
+	
+}
