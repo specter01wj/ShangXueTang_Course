45530c013a80ca9079c3c248169ce4dd61a7e87b
specter01wj <specter01wj@gmail.com>
2019-01-10 00:07:54 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestString.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestString.java
@@ -14,6 +14,7 @@ public class TestString {
 		String str7 = new String("James");
 		System.out.println("str5 =? str6: " + (str5 == str6));
 		System.out.println("str6 =? str7: " + (str6 == str7));
+		System.out.println("str6 equals str7: " + (str6.equals(str7)));
 		
 	}
 	
