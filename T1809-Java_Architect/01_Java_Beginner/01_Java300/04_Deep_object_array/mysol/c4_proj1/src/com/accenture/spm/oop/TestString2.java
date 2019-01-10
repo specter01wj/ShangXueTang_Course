e138df94da6ab10378b5966659d08e4c32fb4a14
specter01wj <specter01wj@gmail.com>
2019-01-10 02:02:24 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestString2.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestString2.java
@@ -8,6 +8,16 @@ public class TestString2 {
 		
 		System.out.println(s1.charAt(3));
 		System.out.println(s2.length());
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(s2));
+		
+		System.out.println(s1.indexOf("Java"));
+		System.out.println(s1.indexOf("apple"));
+		
+		String s = s1.replace(' ', '$');
+		System.out.println(s);
+		
 	}
 	
 }
