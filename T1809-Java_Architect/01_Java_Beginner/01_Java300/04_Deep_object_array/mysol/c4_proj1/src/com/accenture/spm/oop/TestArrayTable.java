c1e4ba12d1f76a8d32a9c2be4377fd3c619ecc85
specter01wj <specter01wj@gmail.com>
2019-01-11 17:05:16 -0500

    01/07/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestArrayTable.java
@@ -0,0 +1,22 @@
+package com.accenture.spm.oop;
+
+import java.util.Arrays;
+
+public class TestArrayTable {
+
+	public static void main(String[] args) {
+		
+		Object[] emp1 = {1001, "James", 23, "Engineer", "2016-02-13" };
+		Object[] emp2 = {1002, "Kim", 27, "Accountant", "2014-06-11" };
+		Object[] emp3 = {1003, "George", 34, "Manager", "2008-11-01" };
+		
+		Object[][] tableData = new Object[3][];
+		tableData[0] = emp1;
+		tableData[1] = emp2;
+		tableData[2] = emp3;
+		
+		System.out.println(Arrays.toString(tableData));
+		
+	}
+	
+}
