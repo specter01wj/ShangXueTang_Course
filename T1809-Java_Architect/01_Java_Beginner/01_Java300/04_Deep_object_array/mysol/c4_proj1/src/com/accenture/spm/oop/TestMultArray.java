232b89a413ec3f9473eab21aabb15ed1a173d3fe
specter01wj <specter01wj@gmail.com>
2019-01-11 16:56:34 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestMultArray.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestMultArray.java
@@ -10,7 +10,13 @@ public class TestMultArray {
 		a1[1] = new int[] {10,15,80};
 		a1[2] = new int[] {50,60};
 		
-		System.out.println(a1[1][2]);
+		int[][] b1 = {
+				{20,30,40},
+				{50,20},
+				{100,200,300,400},
+		};
+		
+		System.out.println("a1: " + a1[1][2] + " / b1: " + b1[2][3]);
 		
 	}
 	
