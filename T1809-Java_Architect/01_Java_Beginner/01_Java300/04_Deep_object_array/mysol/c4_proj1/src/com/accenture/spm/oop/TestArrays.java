a455dabe3e30ecbc83e47320459e2e95dbcae181
specter01wj <specter01wj@gmail.com>
2019-01-11 12:30:24 -0500

    01/07/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestArrays.java
@@ -0,0 +1,17 @@
+package com.accenture.spm.oop;
+
+import java.util.Arrays;
+
+public class TestArrays {
+
+	public static void main(String[] args) {
+		
+		int[] a = {10, 20, 30};
+		
+		System.out.println(a);
+		
+		System.out.println(Arrays.toString(a));
+		
+	}
+	
+}
