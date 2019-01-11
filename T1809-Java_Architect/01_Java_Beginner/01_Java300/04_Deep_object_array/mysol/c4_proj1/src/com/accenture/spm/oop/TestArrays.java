01d97881ae6129a2b8c5743ee417ce4493e3527a
specter01wj <specter01wj@gmail.com>
2019-01-11 12:37:14 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestArrays.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestArrays.java
@@ -6,11 +6,15 @@ public class TestArrays {
 
 	public static void main(String[] args) {
 		
-		int[] a = {10, 20, 30};
+		int[] a1 = {10, 20, 30}, a2 = {13, 61, 51, 6, 141, 41, 3, 11};
 		
-		System.out.println(a);
+		System.out.println(a1);
 		
-		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(a1));
+		
+		Arrays.sort(a2);
+		System.out.println(Arrays.toString(a2));
+		
 		
 	}
 	
