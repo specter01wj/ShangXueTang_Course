ecb4269ddab1419e7f4025ee325621cbd9a2ecf7
specter01wj <specter01wj@gmail.com>
2019-01-11 16:54:23 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestMultArray.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestMultArray.java
@@ -4,7 +4,13 @@ public class TestMultArray {
 
 	public static void main(String[] args) {
 		
+		int[][] a1 = new int[3][];
 		
+		a1[0] = new int[] {20,30};
+		a1[1] = new int[] {10,15,80};
+		a1[2] = new int[] {50,60};
+		
+		System.out.println(a1[1][2]);
 		
 	}
 	
