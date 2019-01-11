5d705bed663432961179b7ad084089bf234b5991
specter01wj <specter01wj@gmail.com>
2019-01-11 01:33:20 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestArrayCopy.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestArrayCopy.java
@@ -4,6 +4,8 @@ public class TestArrayCopy {
 
 	public static void main(String[] args) {
 		
+		testBasicCopy();
+		testBasicCopy2();
 		
 	}
 	
@@ -15,6 +17,18 @@ public class TestArrayCopy {
 		
 		for(int i = 0; i < s2.length; i++) {
 			System.out.println(i + "--" + s2[i]);
+		}
+	}
+	
+	public static void testBasicCopy2() {
+		String[] s1 = {"aa", "bb", "cc", "dd", "ee"};
+		//String[] s2 = new String[5];
+		
+		System.arraycopy(s1, 3, s1, 3-1, s1.length - 3);
+		s1[s1.length - 1] = null;
+		
+		for(int i = 0; i < s1.length; i++) {
+			System.out.println(i + "--" + s1[i]);
 		}
 	}
 	
