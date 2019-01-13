574fe0f6a3517f3b7b9fe3ecf2e925f6b9e29818
specter01wj <specter01wj@gmail.com>
2019-01-13 01:22:12 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBinarySearch.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBinarySearch.java
@@ -1,8 +1,12 @@
 package com.accenture.spm.oop;
 
+import java.util.Arrays;
+
 public class TestBinarySearch {
 
 	public static void main(String[] args) {
+		int[] arr = {30, 20, 50, 10, 80, 9, 7, 12, 100, 40, 8};
+		Arrays.sort(arr);
 		
 		
 		
