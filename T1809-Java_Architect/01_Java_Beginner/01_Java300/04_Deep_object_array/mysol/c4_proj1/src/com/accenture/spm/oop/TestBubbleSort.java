5d7abf9ee5e86c7c6b0edcb82bede1ef239d7378
specter01wj <specter01wj@gmail.com>
2019-01-13 00:14:25 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBubbleSort.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBubbleSort.java
@@ -9,16 +9,18 @@ public class TestBubbleSort {
 		int[] values = {3, 1, 6, 2, 9, 0, 7, 4, 5, 8};
 		int temp = 0;
 		
-		for(int i = 0;i < values.length-1;i++) {
-			
-			if(values[i] > values[i+1]) {
-				temp = values[i];
-				values[i] = values[i+1];
-				values[i+1] = temp;
+		for(int j = 0;j < values.length - 1;j++) {
+			for(int i = 0;i < values.length-1-j;i++) {
+				if(values[i] > values[i+1]) {
+					temp = values[i];
+					values[i] = values[i+1];
+					values[i+1] = temp;
+				}
+				System.out.println(Arrays.toString(values));
 			}
-			System.out.println(Arrays.toString(values));
-			
+			System.out.println("=======================");
 		}
+		
 		
 		
 		
