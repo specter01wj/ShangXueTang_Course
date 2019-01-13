fb1876d18e13aeb5a3c62c8b23560e995f9954b4
specter01wj <specter01wj@gmail.com>
2019-01-13 01:40:24 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBinarySearch.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBinarySearch.java
@@ -5,10 +5,38 @@ import java.util.Arrays;
 public class TestBinarySearch {
 
 	public static void main(String[] args) {
-		int[] arr = {30, 20, 50, 10, 80, 9, 7, 12, 100, 40, 8};
+		int[] arr = {30, 20, 50, 10, 80, 9, 7, 12, 100, 40, 8, 88};
 		Arrays.sort(arr);
 		
+		System.out.println(Arrays.toString(arr));
+		int result = myBinarySearch(arr, 40);
+		System.out.println("Result: " + result);
 		
+		
+	}
+	
+	public static int myBinarySearch(int[] arr, int value) {
+		
+		int low = 0, high = arr.length - 1;
+		
+		
+		while(low <= high) {
+			
+			int mid = (low + high) / 2;
+			
+			if(value == arr[mid]) {
+				return mid;
+			}
+			
+			if(value >= arr[mid]) {
+				low = mid + 1;
+			}
+			if(value <= arr[mid]) {
+				high = mid - 1;
+			}
+			
+		}
+		return -1;
 		
 	}
 	
