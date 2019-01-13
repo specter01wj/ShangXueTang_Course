f2d69c4713bb16dd738074052b4fc50ac474228c
specter01wj <specter01wj@gmail.com>
2019-01-12 23:53:51 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBubbleSort.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/04_Deep_object_array/mysol/c4_proj1/src/com/accenture/spm/oop/TestBubbleSort.java
@@ -5,8 +5,11 @@ public class TestBubbleSort {
 	public static void main(String[] args) {
 		
 		int[] values = {3, 1, 6, 2, 9, 0, 7, 4, 5, 8};
+		int temp = 0;
 		
-		
+		for(int i = 0;j < values.length;j++) {
+			
+		}
 		
 	}
 	
