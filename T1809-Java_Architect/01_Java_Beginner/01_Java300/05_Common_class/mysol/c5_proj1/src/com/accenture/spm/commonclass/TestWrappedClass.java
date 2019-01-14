74920936784d0f42f621c855fe8403f0d84958da
specter01wj <specter01wj@gmail.com>
2019-01-13 23:53:16 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestWrappedClass.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestWrappedClass.java
@@ -4,6 +4,8 @@ public class TestWrappedClass {
 
 	public static void main(String[] args) {
 		
+		Integer a1 = new Integer(3);
+		Integer b1 = Integer.valueOf(30);
 		
 		System.out.println("Result: ");
 	}
