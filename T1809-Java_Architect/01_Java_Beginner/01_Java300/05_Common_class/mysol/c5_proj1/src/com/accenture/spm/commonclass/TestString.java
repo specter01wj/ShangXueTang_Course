f4249c5429165899ef4caaca5fc62235f61e0a5f
specter01wj <specter01wj@gmail.com>
2019-01-14 02:28:38 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestString.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestString.java
@@ -8,6 +8,10 @@ public class TestString {
 		
 		System.out.println(str1 + " / " + str2);
 		
+		String str3 = "WJ" + " LEE", str4 = "WJ LEE";
+		System.out.println(str3 == str4);
+		String str5 = "WJ", str6 = " LEE";
+		System.out.println(str5 == str6);
 		
 	}
 	
