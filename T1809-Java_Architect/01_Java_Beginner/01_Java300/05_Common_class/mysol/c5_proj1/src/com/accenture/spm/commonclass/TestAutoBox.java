b063dd07f5525662daf2a39776cfce3b8cc90e92
specter01wj <specter01wj@gmail.com>
2019-01-14 00:41:13 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestAutoBox.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestAutoBox.java
@@ -10,6 +10,17 @@ public class TestAutoBox {
 		int b1 = a1;
 		int b2 = a1.intValue();
 		
+		//Integer in1 = -128;
+		Integer in1 = Integer.valueOf(-128);
+		Integer in2 = -128;
+		System.out.println(in1 == in2);
+		System.out.println(in1.equals(in2));
+		
+		Integer in3 = 1234;
+		Integer in4 = 1234;
+		System.out.println(in3 == in4);
+		System.out.println(in3.equals(in4));
+		
 	}
 	
 }
