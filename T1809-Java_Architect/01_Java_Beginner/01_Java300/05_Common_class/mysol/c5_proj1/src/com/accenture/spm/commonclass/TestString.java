a669be4c5e73c4ae7488ff1d4cda75b364d7709a
specter01wj <specter01wj@gmail.com>
2019-01-14 02:23:44 -0500

    01/05/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestString.java
@@ -0,0 +1,14 @@
+package com.accenture.spm.commonclass;
+
+public class TestString {
+
+	public static void main(String[] args) {
+	
+		String str1 = "abcdefg", str2 = str1.substring(2, 5);
+		
+		System.out.println(str1 + " / " + str2);
+		
+		
+	}
+	
+}
