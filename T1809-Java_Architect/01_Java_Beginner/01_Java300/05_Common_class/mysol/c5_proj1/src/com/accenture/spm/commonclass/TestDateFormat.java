b2753e8f1a2eb0c43ab1c6a4bebd7358f88c3115
specter01wj <specter01wj@gmail.com>
2019-01-15 18:31:02 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDateFormat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDateFormat.java
@@ -10,14 +10,15 @@ public class TestDateFormat {
 	public static void main(String[] args) throws ParseException {
 		
 		DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-		
 		String str1 = df1.format(new Date(400000000));
-		
 		System.out.println(str1);
 		
 		DateFormat df2 = new SimpleDateFormat("MM-dd-yy hh:mm:ss");
 		Date date = df2.parse("05-14-98 23:12:33");
 		System.out.println(date);
+		
+		
+		
 	}
 	
 }
