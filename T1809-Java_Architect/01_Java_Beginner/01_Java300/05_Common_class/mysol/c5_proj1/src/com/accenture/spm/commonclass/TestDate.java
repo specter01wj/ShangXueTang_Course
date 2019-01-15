a41d33e4435b2a20172ad714c131defb504d6e5e
specter01wj <specter01wj@gmail.com>
2019-01-15 01:31:56 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDate.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDate.java
@@ -1,10 +1,14 @@
 package com.accenture.spm.commonclass;
 
+import java.util.Date;
+
 public class TestDate {
 
 	public static void main(String[] args) {
 		
+		Date d1 = new Date();
 		
+		System.out.println(d1);
 		
 	}
 	
