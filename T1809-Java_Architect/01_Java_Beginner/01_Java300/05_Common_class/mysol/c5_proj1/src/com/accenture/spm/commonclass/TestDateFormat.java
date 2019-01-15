68322e3c7a80feec038e239e8aba55e0f5bfc0e1
specter01wj <specter01wj@gmail.com>
2019-01-15 18:22:34 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDateFormat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDateFormat.java
@@ -1,13 +1,18 @@
 package com.accenture.spm.commonclass;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestDateFormat {
 
 	public static void main(String[] args) {
 		
-		DateFormat df1;
+		DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		
+		String str1 = df1.format(new Date(400000000));
+		
+		System.out.println(str1);
 	}
 	
 }
