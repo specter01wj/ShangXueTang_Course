e17d48382b170cdb92fb99423d63be9c16891175
specter01wj <specter01wj@gmail.com>
2019-01-15 01:38:02 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDate.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestDate.java
@@ -6,10 +6,16 @@ public class TestDate {
 
 	public static void main(String[] args) {
 		
-		Date d1 = new Date();
+		Date d1 = new Date(2000);
 		
 		System.out.println(d1);
 		
+		System.out.println(d1.getTime());
+		
+		Date d2 = new Date();
+		System.out.println(d2.getTime());
+		
+		System.out.println(d2.after(d1));
 	}
 	
 }
