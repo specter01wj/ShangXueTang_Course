995705be72072a087776de8fa2e6ad884cf36254
specter01wj <specter01wj@gmail.com>
2019-01-14 23:55:02 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestStringBuilder3.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestStringBuilder3.java
@@ -4,7 +4,17 @@ public class TestStringBuilder3 {
 
 	public static void main(String[] args) {
 		
+		String str1 = "";
+		long num1 = Runtime.getRuntime().freeMemory();
+		long time1 = System.currentTimeMillis();
+		for(int i = 0; i < 5000; i++) {
+			str1 += i;
+		}
 		
+		long num2 = Runtime.getRuntime().freeMemory();
+		long time2 = System.currentTimeMillis();
+		System.out.println("String Memory: " + (num1 - num2));
+		System.out.println("String Time: " + (time2 - time1));
 		
 	}
 	
