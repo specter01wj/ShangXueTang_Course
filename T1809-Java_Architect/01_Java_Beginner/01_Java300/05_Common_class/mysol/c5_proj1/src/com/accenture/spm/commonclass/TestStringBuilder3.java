b3f1a341925b0fe5fdde4b8787a5a198bf14af32
specter01wj <specter01wj@gmail.com>
2019-01-14 23:59:20 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestStringBuilder3.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestStringBuilder3.java
@@ -7,14 +7,26 @@ public class TestStringBuilder3 {
 		String str1 = "";
 		long num1 = Runtime.getRuntime().freeMemory();
 		long time1 = System.currentTimeMillis();
-		for(int i = 0; i < 5000; i++) {
+		for(int i = 0; i < 500; i++) {
 			str1 += i;
 		}
-		
+		System.out.println("CPU Memory: " + (num1));
 		long num2 = Runtime.getRuntime().freeMemory();
 		long time2 = System.currentTimeMillis();
 		System.out.println("String Memory: " + (num1 - num2));
 		System.out.println("String Time: " + (time2 - time1));
+		
+		StringBuilder sb1 = new StringBuilder("");
+		long num3 = Runtime.getRuntime().freeMemory();
+		long time3 = System.currentTimeMillis();
+		for(int i = 0; i < 500; i++) {
+			sb1.append(i);
+		}
+		System.out.println("CPU Memory: " + (num3));
+		long num4 = Runtime.getRuntime().freeMemory();
+		long time4 = System.currentTimeMillis();
+		System.out.println("StringBuilder Memory: " + (num3 - num4));
+		System.out.println("StringBuilder Time: " + (time4 - time3));
 		
 	}
 	
