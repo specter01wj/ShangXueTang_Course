50884c121dcdb45e60517b32f89f4445e613bf3c
specter01wj <specter01wj@gmail.com>
2019-01-16 16:03:24 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestCalendar2.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestCalendar2.java
@@ -3,7 +3,9 @@ package com.accenture.spm.commonclass;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class TestCalendar2 {
 
@@ -12,6 +14,8 @@ public class TestCalendar2 {
 		String str1 = "2020-10-23";
 		DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
 		Date date1 = df1.parse(str1);
+		Calendar cal1 = new GregorianCalendar();
+		cal1.setTime(date1);
 		
 	}
 	
