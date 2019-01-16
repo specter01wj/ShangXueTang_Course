7175b2a03a3aedf460f2177227ee05cbb0c1c90f
specter01wj <specter01wj@gmail.com>
2019-01-16 01:45:46 -0500

    01/15/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestCalendar.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestCalendar.java
@@ -1,10 +1,13 @@
 package com.accenture.spm.commonclass;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TestCalendar {
 
 	public static void main(String[] args) {
 		
-		
+		Calendar cal1 = new GregorianCalendar();
 		
 		
 	}
