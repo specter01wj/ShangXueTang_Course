0a4c97ac1db551a345a3e2f28f0b4b79f0612558
specter01wj <specter01wj@gmail.com>
2019-01-16 01:59:39 -0500

    01/15/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestCalendar.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestCalendar.java
@@ -7,8 +7,21 @@ public class TestCalendar {
 
 	public static void main(String[] args) {
 		
-		Calendar cal1 = new GregorianCalendar();
+		Calendar cal1 = new GregorianCalendar(2020, 10, 12, 21, 11, 35);
+		int year1 = cal1.get(Calendar.YEAR);
+		int month1 = cal1.get(Calendar.MONTH);
+		int day1 = cal1.get(Calendar.DATE);
+		int weekday1 = cal1.get(Calendar.DAY_OF_WEEK);
 		
+		System.out.println(year1 + " / " + month1 + " / " + weekday1 + " / " + day1);
+	
+		Calendar cal2 = new GregorianCalendar();
+		cal2.set(Calendar.YEAR, 2023);
+		System.out.println(cal2);
+		
+		Calendar cal3 = new GregorianCalendar();
+		cal2.add(Calendar.DATE, 100);
+		System.out.println(cal3);
 		
 	}
 	
