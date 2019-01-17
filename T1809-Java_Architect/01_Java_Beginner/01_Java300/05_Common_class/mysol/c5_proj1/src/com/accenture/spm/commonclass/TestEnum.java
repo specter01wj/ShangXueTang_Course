a4110403c08d48cb85f541bac8d443fec4d8a040
specter01wj <specter01wj@gmail.com>
2019-01-17 18:47:56 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestEnum.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestEnum.java
@@ -5,15 +5,35 @@ import java.io.IOException;
 public class TestEnum {
 
 	public static void main(String[] args) {
+		System.out.println(Season.SUMMER);
 		
+		Season a1 = Season.AUTUMN;
+		Season b1 = Season.UNKNOWN;
 		
+		switch(a1) {
+		case SPRING:
+			System.out.println("Spring coming!");
+			break;
+		case SUMMER:
+			System.out.println("Summer coming!");
+			break;
+		case AUTUMN:
+			System.out.println("Autumn coming!");
+			break;
+		case WINTER:
+			System.out.println("Winter coming!");
+			break;
+		default:
+			System.out.println("Stay Home?");
+			break;
+		}
 		
 	}
 	
 }
 
 enum Season {
-	SPRING, SUMMER, AUTUMN, WINTER
+	SPRING, SUMMER, AUTUMN, WINTER, UNKNOWN
 }
 
 enum Week {
