491e046c734ab6a2f654437aeb485e58125644f4
specter01wj <specter01wj@gmail.com>
2019-01-17 03:21:29 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestFile.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestFile.java
@@ -2,6 +2,7 @@ package com.accenture.spm.commonclass;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 public class TestFile {
 
@@ -13,6 +14,17 @@ public class TestFile {
 		file1.renameTo(new File("files/bb1.txt")); 
 		
 		System.out.println(System.getProperty("user.dir"));
+		
+		File f2 = new File("gg.txt");
+		f2.createNewFile();
+		
+		System.out.println("File if exist: "+f2.exists());
+        System.out.println("File if dir: "+f2.isDirectory());
+        System.out.println("File if file: "+f2.isFile());
+        System.out.println("File last mod: "+new Date(f2.lastModified()));
+        System.out.println("File size: "+f2.length());
+        System.out.println("File name: "+f2.getName());
+        System.out.println("File dir: "+f2.getAbsolutePath());
 	}
 	
 }
