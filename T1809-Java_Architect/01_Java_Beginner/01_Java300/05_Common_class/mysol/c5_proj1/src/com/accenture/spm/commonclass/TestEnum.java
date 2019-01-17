38a5d9b494614b1e2ea2a3b459b47d949f9e89e3
specter01wj <specter01wj@gmail.com>
2019-01-17 18:41:39 -0500

    01/07/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/TestEnum.java
@@ -0,0 +1,21 @@
+package com.accenture.spm.commonclass;
+
+import java.io.IOException;
+
+public class TestEnum {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+
+enum Season {
+	SPRING, SUMMER, AUTUMN, WINTER
+}
+
+enum Week {
+	MON, TUE, WED, THUR, FRI, SAT, SUN
+}
