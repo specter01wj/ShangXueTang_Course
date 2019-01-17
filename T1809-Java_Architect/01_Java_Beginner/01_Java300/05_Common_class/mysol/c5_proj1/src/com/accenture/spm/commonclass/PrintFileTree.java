401adf86b7becda116c323b164f47055cf8e1419
specter01wj <specter01wj@gmail.com>
2019-01-17 18:32:47 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/PrintFileTree.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/PrintFileTree.java
@@ -1,11 +1,26 @@
 package com.accenture.spm.commonclass;
 
+import java.io.File;
+
 public class PrintFileTree {
 
 	public static void main(String[] args) {
 		
+		File file1 = new File("files");
 		
+		printFile(file1);
 		
+	}
+	
+	static void printFile(File file) {
+		System.out.println(file.getName());
+		
+		if(file.isDirectory()) {
+			File[] files = file.listFiles();
+			for(File temp : files) {
+				printFile(temp);
+			}
+		}
 	}
 	
 }
