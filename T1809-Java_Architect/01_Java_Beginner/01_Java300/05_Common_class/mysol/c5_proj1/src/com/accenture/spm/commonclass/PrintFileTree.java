b5c25736f6eba3bb65665fed3aa7f3c88c55e34f
specter01wj <specter01wj@gmail.com>
2019-01-17 18:37:36 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/PrintFileTree.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/05_Common_class/mysol/c5_proj1/src/com/accenture/spm/commonclass/PrintFileTree.java
@@ -8,17 +8,20 @@ public class PrintFileTree {
 		
 		File file1 = new File("files");
 		
-		printFile(file1);
+		printFile(file1, 0);
 		
 	}
 	
-	static void printFile(File file) {
+	static void printFile(File file, int level) {
+		for(int i = 0; i < level; i++) {
+			System.out.print("-");
+		}
 		System.out.println(file.getName());
 		
 		if(file.isDirectory()) {
 			File[] files = file.listFiles();
 			for(File temp : files) {
-				printFile(temp);
+				printFile(temp, level+1);
 			}
 		}
 	}
