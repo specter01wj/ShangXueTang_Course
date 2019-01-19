53a53e370cb295d9f5126fb5a4b8f7869888f45e
specter01wj <specter01wj@gmail.com>
2019-01-19 18:45:50 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestIOException.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestIOException.java
@@ -1,19 +1,24 @@
 package com.accenture.spm.exception;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class TestIOException {
 
 	public static void main(String[] args) {
 		
-		FileReader redaer = null;
+		FileReader reader = null;
 		
 		try {
-			reader = new FileReader("")
-		} catch() {
-			
-		} catch() {
-			
+			reader = new FileReader("bb1.txt");
+			char c1 = (char) reader.read();
+			char c2 = (char) reader.read();
+			System.out.println("" + c1 + c2);
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
 		} finally {
 			
 		}
