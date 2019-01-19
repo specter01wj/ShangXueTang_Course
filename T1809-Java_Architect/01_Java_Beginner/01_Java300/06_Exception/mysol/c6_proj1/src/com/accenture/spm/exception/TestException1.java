f6cdb8c66ccecdc3920b59d2b3b6f57502985269
specter01wj <specter01wj@gmail.com>
2019-01-19 13:16:24 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestException1.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestException1.java
@@ -4,9 +4,15 @@ public class TestException1 {
 
 	public static void main(String[] args) {
 		
-		int a = 1/0;
-		System.out.println(a);
+		//int a = 1/0;
+		int a = 0, b = 1;
 		
+		if(a!=0) {
+			System.out.println(b/a);
+		}
+		
+		String str = null;
+		str.length();
 		
 	}
 	
