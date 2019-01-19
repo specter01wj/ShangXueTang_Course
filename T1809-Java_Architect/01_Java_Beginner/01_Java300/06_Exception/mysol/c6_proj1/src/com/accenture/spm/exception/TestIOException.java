c4abf55b9d9e64d6c467dbbc46f4c11dcb170f98
specter01wj <specter01wj@gmail.com>
2019-01-19 18:42:57 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestIOException.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestIOException.java
@@ -1,10 +1,22 @@
 package com.accenture.spm.exception;
 
+import java.io.FileReader;
+
 public class TestIOException {
 
 	public static void main(String[] args) {
 		
+		FileReader redaer = null;
 		
+		try {
+			reader = new FileReader("")
+		} catch() {
+			
+		} catch() {
+			
+		} finally {
+			
+		}
 		
 	}
 	
