8661aba2782e7d15679c142cc6cd04d9bed22fab
specter01wj <specter01wj@gmail.com>
2019-01-20 14:20:38 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestGeneric.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestGeneric.java
@@ -4,30 +4,43 @@ public class TestGeneric {
 
 	public static void main(String[] args) {
 		
-		MyCollection mc1 = new MyCollection();
+		//MyCollection mc1 = new MyCollection();
+		MyCollection<String> mc1 = new MyCollection();
 		mc1.setObjs("James", 0);
-		mc1.setObjs(1011, 1);
+		//mc1.setObjs(1011, 1);
 		
-		String a1 = (String)mc1.getObjs(0);
-		Integer b1 = (Integer)mc1.getObjs(1);
+		//String a1 = (String)mc1.getObjs(0);
+		//Integer b1 = (Integer)mc1.getObjs(1);
 		System.out.println(mc1.getObjs(1));
 		System.out.println("/" + a1 + "/" + b1);
 	}
 	
 }
 
-class MyCollection {
+/*class MyCollection {
 	
 	Object[] objs = new Object[5];
-
+	
 	public Object getObjs(int index) {
 		return objs[index];
 	}
-
+	
 	public void setObjs(Object objs, int index) {
 		this.objs[index] = objs;
 	}
 	
+}*/
+
+class MyCollection<E> {
 	
+	Object[] objs = new Object[5];
+
+	public E getObjs(int index) {
+		return (E)objs[index];
+	}
+
+	public void setObjs(E e, int index) {
+		this.objs[index] = e;
+	}
 	
 }
