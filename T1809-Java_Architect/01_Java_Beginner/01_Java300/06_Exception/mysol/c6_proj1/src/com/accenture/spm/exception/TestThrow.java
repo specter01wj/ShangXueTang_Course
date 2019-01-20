c9e1b9327533703c9480f9bf972819436a6c08ba
specter01wj <specter01wj@gmail.com>
2019-01-19 19:04:00 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestThrow.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestThrow.java
@@ -1,7 +1,30 @@
 package com.accenture.spm.exception;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TestThrow {
 
+	public static void main(String[] args) throws IOException {
+		
+		readMyFile();
+		
+	}
 	
+	public static void readMyFile() throws IOException {
+		FileReader reader = null;
+		
+		reader = new FileReader("bb1.txt");
+		char c1 = (char) reader.read();
+		char c2 = (char) reader.read();
+		System.out.println("Read file content: " + c1 + c2);
+		
+		if(reader != null) {
+			System.out.println("File close!");
+			reader.close();
+		}
+	
+	}
 	
 }
