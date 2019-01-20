0721d557feca478b222bb887291864c5f763a442
specter01wj <specter01wj@gmail.com>
2019-01-19 19:30:15 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestSelfException.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/06_Exception/mysol/c6_proj1/src/com/accenture/spm/exception/TestSelfException.java
@@ -9,3 +9,15 @@ public class TestSelfException {
 	}
 	
 }
+
+class IllegalAgeException extends RuntimeException {
+	
+	public IllegalAgeException() {
+		
+	}
+	
+	public IllegalAgeException(String msg) {
+		super(msg);
+	}
+	
+}
