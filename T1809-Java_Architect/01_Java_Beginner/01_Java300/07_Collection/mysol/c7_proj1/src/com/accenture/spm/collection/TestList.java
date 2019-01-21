56a9a5af499c4b3c094564079241102d054043ea
specter01wj <specter01wj@gmail.com>
2019-01-21 00:30:34 -0500

    01/07/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestList.java
@@ -0,0 +1,13 @@
+package com.accenture.spm.collection;
+
+import java.util.*;
+
+public class TestList {
+
+	public static void main(String[] args) {
+		
+		Collection c1 = new ArrayList();
+		
+	}
+	
+}
