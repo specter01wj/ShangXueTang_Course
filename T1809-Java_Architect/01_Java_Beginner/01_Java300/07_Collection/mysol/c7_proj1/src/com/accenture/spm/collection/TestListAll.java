f4f9bf140c99b693c82bfa703a4a8ee1bb669db9
specter01wj <specter01wj@gmail.com>
2019-01-21 02:28:38 -0500

    01/07/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestListAll.java
@@ -0,0 +1,27 @@
+package com.accenture.spm.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestListAll {
+
+	public static void main(String[] args) {
+		
+		test02();
+		
+	}
+	
+	public static void test01() {
+		
+	}
+	
+	public static void test02() {
+		
+		List<String> list1 = new ArrayList<>();
+		list1.add("aa1");
+		list1.add("aa2");
+		list1.add("aa3");
+		
+	}
+	
+}
