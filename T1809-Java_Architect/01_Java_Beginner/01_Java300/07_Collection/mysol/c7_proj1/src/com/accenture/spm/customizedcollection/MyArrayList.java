a1290b11b392d2a89df0425ed6968250c7a56a2c
specter01wj <specter01wj@gmail.com>
2019-01-21 22:54:59 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayList.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayList.java
@@ -4,10 +4,15 @@ public class MyArrayList {
 	
 	private Object[] elementData;
 	private int size;
-	
+	private static final int DEFAULT_CAPACITY = 10;
 	
 	public MyArrayList() {
-		elementData = new Object[10];
+		elementData = new Object[DEFAULT_CAPACITY];
+		
+	}
+	
+	public MyArrayList(int capacity) {
+		elementData = new Object[capacity];
 		
 	}
 	
