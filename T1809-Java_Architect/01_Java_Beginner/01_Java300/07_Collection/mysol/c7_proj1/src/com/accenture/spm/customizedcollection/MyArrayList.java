0544e9ef274641e33f2b8b01fe979188236c273b
specter01wj <specter01wj@gmail.com>
2019-01-21 22:52:06 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayList.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayList.java
@@ -1,8 +1,22 @@
 package com.accenture.spm.customizedcollection;
 
 public class MyArrayList {
+	
+	private Object[] elementData;
+	private int size;
+	
+	
+	public MyArrayList() {
+		elementData = new Object[10];
+		
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
+		
+		
 		
 		
 		
