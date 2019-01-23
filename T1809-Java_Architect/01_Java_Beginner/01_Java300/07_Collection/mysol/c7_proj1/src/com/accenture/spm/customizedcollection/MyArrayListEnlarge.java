82c2ed960f82cedc26a4181e0c4a956a786d8a0f
specter01wj <specter01wj@gmail.com>
2019-01-23 01:36:25 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayListEnlarge.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayListEnlarge.java
@@ -1,10 +1,50 @@
 package com.accenture.spm.customizedcollection;
 
-public class MyArrayListEnlarge {
+public class MyArrayListEnlarge<E> {
+	
+	private Object[] elementData;
+	private int size;
+	private static final int DEFAULT_CAPACITY = 10;
+	
+	public MyArrayListEnlarge() {
+		elementData = new Object[DEFAULT_CAPACITY];
+		
+	}
+	
+	public MyArrayListEnlarge(int capacity) {
+		elementData = new Object[capacity];
+		
+	}
+	
+	public void add(E element) {
+		elementData[size++] = element;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("[");
+		/*for(Object obj : elementData) {
+			sb.append(obj + ",");
+		}*/
+		for(int i = 0; i < size; i++) {
+			sb.append(elementData[i] + ",");
+		}
+		sb.setCharAt(sb.length()-1, ']');
+		//sb.append("]");
+		
+		return sb.toString();
+	}
 
 	public static void main(String[] args) {
 		
+		MyArrayListEnlarge s1 = new MyArrayListEnlarge(20);
+		s1.add("aa1");
+		s1.add("bb1");
 		
+		
+		System.out.println(s1);
 		
 	}
 	
