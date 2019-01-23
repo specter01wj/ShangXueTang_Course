b04257bb0cad02afa97a724bcabf8e50ac103b82
specter01wj <specter01wj@gmail.com>
2019-01-23 18:11:32 -0500

    01/07/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyArrayListSetGet.java
@@ -0,0 +1,59 @@
+package com.accenture.spm.customizedcollection;
+
+public class MyArrayListSetGet<E> {
+
+	private Object[] elementData;
+	private int size;
+	private static final int DEFAULT_CAPACITY = 10;
+	
+	public MyArrayListSetGet() {
+		elementData = new Object[DEFAULT_CAPACITY];
+		
+	}
+	
+	public MyArrayListSetGet(int capacity) {
+		elementData = new Object[capacity];
+		
+	}
+	
+	public void add(E element) {
+		
+		if(size == elementData.length) {
+			Object[] newArray = new Object[elementData.length + (elementData.length >> 1)];
+			System.arraycopy(elementData, 0, newArray, 0, elementData.length);
+			elementData = newArray;
+		}
+		
+		elementData[size++] = element;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("[");
+		/*for(Object obj : elementData) {
+			sb.append(obj + ",");
+		}*/
+		for(int i = 0; i < size; i++) {
+			sb.append(elementData[i] + ",");
+		}
+		sb.setCharAt(sb.length()-1, ']');
+		//sb.append("]");
+		
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		
+		MyArrayListSetGet s1 = new MyArrayListSetGet(20);
+		
+		for(int i = 0; i < 40; i++) {
+			s1.add("We_" + i);
+		}
+		
+		System.out.println(s1);
+		
+	}
+	
+}
