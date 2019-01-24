06e47eec20a79fb6a79cab9c5645c72046a89943
specter01wj <specter01wj@gmail.com>
2019-01-24 17:58:49 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/Node.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/Node.java
@@ -2,6 +2,17 @@ package com.accenture.spm.customizedcollection;
 
 public class Node {
 
+	Node previous, next;
+	Object element;
+	
+	
+	public Node(Node previous, Node next, Object element) {
+		super();
+		this.previous = previous;
+		this.next = next;
+		this.element = element;
+	}
+	
 	
 	
 }
