7b3fc2f4b17a17f47751be82442d039e0ef9ae0f
specter01wj <specter01wj@gmail.com>
2019-01-24 18:12:42 -0500

    01/07/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyLinkedListAdd.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyLinkedListAdd.java
@@ -7,6 +7,25 @@ public class MyLinkedListAdd {
 	
 	public void add(Object obj) {
 		Node node = new Node(obj);
+		
+		if(first == null) {
+			node.previous = null;
+			node.next = null;
+			
+			first = node;
+			last = node;
+		} else {
+			node.previous = last;
+			node.next = null;
+			last.next = node;
+			last = node;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
 	}
 	
 }
