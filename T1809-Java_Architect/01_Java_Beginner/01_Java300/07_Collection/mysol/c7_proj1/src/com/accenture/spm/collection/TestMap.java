f7f46ff42ef30d1b83d27f52de140e0ef8b1e2b5
specter01wj <specter01wj@gmail.com>
2019-01-26 18:17:43 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
@@ -8,12 +8,28 @@ public class TestMap {
 	public static void main(String[] args) {
 		
 		Map<Integer, String> m1 = new HashMap<>();
+		Map<String, Integer> m2 = new HashMap<>();
+		Map<Integer, String> m3 = new HashMap<>();
 		
 		m1.put(1, "one");
 		m1.put(2, "two");
 		m1.put(3, "three");
 		
+		m2.put("James", 1001);
+		m2.put("Kim", 1002);
+		
+		m3.put(3, "three-some");
+		m3.put(4, "four");
+		m3.put(5, "five");
+		
 		System.out.println(m1.get(1));
+		System.out.println(m1.size());
+		System.out.println(m1.isEmpty());
+		System.out.println(m1.containsKey(2));
+		System.out.println(m1.containsValue("two"));
+		
+		m1.putAll(m3);
+		System.out.println(m1);
 		
 	}
 	
