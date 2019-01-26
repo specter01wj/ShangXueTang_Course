6a955921b843a21b02200bf4492a5357a8449875
specter01wj <specter01wj@gmail.com>
2019-01-26 18:05:59 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
@@ -7,7 +7,13 @@ public class TestMap {
 
 	public static void main(String[] args) {
 		
-		Map m1 = new HashMap();
+		Map<Integer, String> m1 = new HashMap<>();
+		
+		m1.put(1, "one");
+		m1.put(2, "two");
+		m1.put(3, "three");
+		
+		System.out.println(m1.get(1));
 		
 	}
 	
