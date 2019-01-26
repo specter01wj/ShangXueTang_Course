a8dab4db488d38cee21285610cc504d5035479d9
specter01wj <specter01wj@gmail.com>
2019-01-26 13:49:58 -0500

    01/25/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
@@ -0,0 +1,11 @@
+package com.accenture.spm.collection;
+
+public class TestMap {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestVector.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestVector.java
@@ -1,5 +1,14 @@
 package com.accenture.spm.collection;
 
+import java.util.List;
+import java.util.Vector;
+
 public class TestVector {
 
+	public static void main(String[] args) {
+		
+		List<String> a1 = new Vector();
+		
+	}
+	
 }
