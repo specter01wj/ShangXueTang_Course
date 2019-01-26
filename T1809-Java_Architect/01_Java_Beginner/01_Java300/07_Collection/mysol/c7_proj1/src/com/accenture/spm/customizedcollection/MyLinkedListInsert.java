1d9f5fdd394ac0885063e0eae6ddc9cc0fafa1ee
specter01wj <specter01wj@gmail.com>
2019-01-26 01:08:23 -0500

    01/25/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyLinkedListInsert.java
@@ -0,0 +1,121 @@
+package com.accenture.spm.customizedcollection;
+
+public class MyLinkedListInsert {
+
+	private Node first, last;
+	private int size;
+	
+	public void insert(int index, Object obj) {
+		
+		
+		
+	}
+
+	public void remove(int index) {
+		
+		Node temp = getNode(index);
+		
+		if(temp != null) {
+			Node up = temp.previous;
+			Node down = temp.next;
+			
+			if(up != null) {
+				up.next = down;
+			}
+			if(down != null) {
+				down.previous = up;
+			}
+			if(index == 0) {
+				first = down;
+			}
+			if(index == size - 1) {
+				last = up;
+			}
+			
+			size--;
+		}
+		
+	}
+	
+	public Object get(int index) {
+		if(index < 0 || index > size - 1) {
+			throw new RuntimeException("LinkedList overflow!" + index);
+		}
+		
+		Node temp = getNode(index);
+		
+		return temp != null ? temp.element : null;
+		
+	}
+	
+	public void add(Object obj) {
+		Node node = new Node(obj);
+		
+		if(first == null) {
+			node.previous = null;
+			node.next = null;
+			
+			first = node;
+			last = node;
+		} else {
+			node.previous = last;
+			node.next = null;
+			last.next = node;
+			last = node;
+		}
+		size++;
+	}
+	
+	public Node getNode(int index) {
+		Node temp = null;
+		
+		if(index <= (size>>1)) {
+			temp = first;
+			for(int i = 0; i < index; i++) {
+				temp = temp.next;
+			}
+		} else {
+			temp = last;
+			for(int i = size - 1; i > index; i--) {
+				temp = temp.previous;
+			}
+		}
+		
+		return temp;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		Node temp = first;
+		
+		while(temp != null) {
+			//System.out.println(temp.element);
+			sb.append(temp.element + ",");
+			temp = temp.next;
+		}
+		sb.setCharAt(sb.length()-1, ']');
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		
+		MyLinkedListInsert list1 = new MyLinkedListInsert();
+		
+		list1.add("a");
+		list1.add("b");
+		list1.add("c");
+		list1.add("d");
+		list1.add("e");
+		list1.add("f");
+		
+		System.out.println(list1);
+		
+		System.out.println(list1.get(5));
+		
+		list1.remove(5);
+		System.out.println(list1);
+		
+	}
+	
+}
