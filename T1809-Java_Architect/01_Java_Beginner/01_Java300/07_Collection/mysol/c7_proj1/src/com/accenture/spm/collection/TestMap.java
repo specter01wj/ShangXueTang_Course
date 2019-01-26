45fb4bb08ed283b81a3a961c41d84e50dbb1769c
specter01wj <specter01wj@gmail.com>
2019-01-26 13:50:32 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestMap.java
@@ -1,10 +1,13 @@
 package com.accenture.spm.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestMap {
 
 	public static void main(String[] args) {
 		
-		
+		Map m1 = new HashMap();
 		
 	}
 	
