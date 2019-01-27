b15aadfd75b4e0fc5a73c94a288e262d1ed72970
specter01wj <specter01wj@gmail.com>
2019-01-27 17:10:09 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashMapPut.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashMapPut.java
@@ -2,6 +2,14 @@ package com.accenture.spm.customizedcollection;
 
 public class MyHashMapPut {
 
+	Node_HashMap[] table;
+	int size;
+	
+	
+	public MyHashMapPut() {
+		this.table = new Node_HashMap[16];
+	}
+	
 	
 	
 }
