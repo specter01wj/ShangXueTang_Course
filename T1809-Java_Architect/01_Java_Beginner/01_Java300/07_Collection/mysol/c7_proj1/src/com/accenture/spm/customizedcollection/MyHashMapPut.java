9c013620c6a35dc6dcc600e656e535a6e60dabd0
specter01wj <specter01wj@gmail.com>
2019-01-27 17:15:32 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashMapPut.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashMapPut.java
@@ -10,6 +10,15 @@ public class MyHashMapPut {
 		this.table = new Node_HashMap[16];
 	}
 	
+	public void put(Object key, Object value) {
+		Node_HashMap newNode1 = new Node_HashMap();
+		newNode1.hash = myHash(key.hashCode(), table.length);
+	}
 	
+	public int myHash(int v, int length) {
+		System.out.println(v & (length - 1));
+		System.out.println(v % (length - 1));
+		return v & (length - 1);
+	}
 	
 }
