214a51f9d09c0999cdb375d5ebd437ba8c5366e1
specter01wj <specter01wj@gmail.com>
2019-01-28 02:41:33 -0500

    01/25/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashMapGeneric.java
@@ -0,0 +1,114 @@
+package com.accenture.spm.customizedcollection;
+
+public class MyHashMapGeneric {
+
+	Node_HashMap[] table;
+	int size;
+	
+	
+	public MyHashMapGeneric() {
+		this.table = new Node_HashMap[16];
+	}
+	
+	public Object get(Object key) {
+		
+		int hash = myHash(key.hashCode(), table.length);
+		Object value = null;
+		
+		if(table[hash] != null) {
+			Node_HashMap temp = table[hash];
+			
+			while(temp != null) {
+				if(temp.key.equals(key)) {
+					value = temp.value;
+					break;
+				} else {
+					temp = temp.next;
+				}
+			}
+		}
+		
+		return value;
+	}
+	
+	public void put(Object key, Object value) {
+		Node_HashMap newNode1 = new Node_HashMap();
+		newNode1.hash = myHash(key.hashCode(), table.length);
+		newNode1.key = key;
+		newNode1.value = value;
+		newNode1.next = null;
+		
+		Node_HashMap temp = table[newNode1.hash];
+		Node_HashMap iterLast = null;
+		boolean keyRepeat = false;
+		
+		if(temp == null) {
+			table[newNode1.hash] = newNode1;
+		} else {
+			while(temp != null) {
+				
+				if(temp.key.equals(key)) {
+					keyRepeat = true;
+					System.out.println("Duplicate key!!!");
+					temp.value = value;
+					break;
+				} else {
+					iterLast = temp;
+					temp = temp.next;
+				}
+				
+			}
+			
+			if(!keyRepeat) {
+				iterLast.next = newNode1;
+			}
+			
+			
+		}
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		StringBuilder sb1 = new StringBuilder("{");
+		
+		for(int i = 0; i < table.length; i++) {
+			Node_HashMap temp = table[i];
+			
+			while(temp != null) {
+				sb1.append(temp.key + ":" + temp.value + ",");
+				temp = temp.next;
+			}
+		}
+		sb1.setCharAt(sb1.length()-1, '}');
+		
+		return sb1.toString();
+	}
+
+	public static void main(String[] args) {
+		
+		MyHashMapGeneric m1 = new MyHashMapGeneric();
+		m1.put(101, "Jim");
+		m1.put(102, "Yebo");
+		m1.put(103, "Zion");
+		m1.put(102, "Kate");
+		
+		m1.put(42, "R1");
+		m1.put(58, "R2");
+		m1.put(74, "R3");
+		
+		System.out.println(m1);
+		
+		
+		System.out.println(m1.get(58));
+		
+		
+	}
+	
+	public static int myHash(int v, int length) {
+		return v & (length - 1);
+	}
+	
+}
