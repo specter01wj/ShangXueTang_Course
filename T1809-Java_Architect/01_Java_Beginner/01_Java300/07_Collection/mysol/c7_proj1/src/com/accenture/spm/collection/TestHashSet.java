6f87be707bd91a155045945435a6e8a24c58b92d
specter01wj <specter01wj@gmail.com>
2019-01-29 18:27:24 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestHashSet.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestHashSet.java
@@ -1,10 +1,13 @@
 package com.accenture.spm.collection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestHashSet {
 
 	public static void main(String[] args) {
 		
-		
+		Set<String> set1 = new HashSet<>();
 		
 	}
 	
