7daebec96f26b384c2a7b368f162bf461c73cc4e
specter01wj <specter01wj@gmail.com>
2019-01-29 02:23:05 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestTreeMap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestTreeMap.java
@@ -9,7 +9,13 @@ public class TestTreeMap {
 		
 		Map<Integer, String> tm1 = new TreeMap<>();
 		
+		tm1.put(1001, "aa1");
+		tm1.put(1002, "bb1");
+		tm1.put(1003, "cc1");
 		
+		for(Integer key : tm1.keySet()) {
+			System.out.println(key + "---" + tm1.get(key));
+		}
 		
 	}
 	
