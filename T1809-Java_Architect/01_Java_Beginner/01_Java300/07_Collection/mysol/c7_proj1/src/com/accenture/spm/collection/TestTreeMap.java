06890d51e1145dcc7bc93b337dea464e0183604a
specter01wj <specter01wj@gmail.com>
2019-01-29 02:10:40 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestTreeMap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestTreeMap.java
@@ -1,8 +1,13 @@
 package com.accenture.spm.collection;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TestTreeMap {
 
 	public static void main(String[] args) {
+		
+		Map<Integer, String> tm1 = new TreeMap<>();
 		
 		
 		
