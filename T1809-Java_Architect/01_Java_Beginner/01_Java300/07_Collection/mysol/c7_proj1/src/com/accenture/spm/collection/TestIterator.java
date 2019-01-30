c66f8ed3e3092bddeb187b5ccb1e591bf2c8722a
specter01wj <specter01wj@gmail.com>
2019-01-30 13:23:17 -0500

    01/25/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestIterator.java
@@ -0,0 +1,20 @@
+package com.accenture.spm.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestIterator {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	public static void testIteratorList() {
+		List<String> list1 = new ArrayList<>();
+		
+		
+	}
+	
+}
