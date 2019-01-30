3537ec1a05d0b5d40ac7b6dc2c8c7490883cda77
specter01wj <specter01wj@gmail.com>
2019-01-30 01:46:03 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashSet.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/customizedcollection/MyHashSet.java
@@ -1,6 +1,19 @@
 package com.accenture.spm.customizedcollection;
 
+import java.util.HashMap;
+
 public class MyHashSet {
+
+	HashMap map1;
+	
+	public MyHashSet() {
+		this.map1 = new HashMap();
+	}
+
+	public void add(Object o) {
+		
+	}
+
 
 	public static void main(String[] args) {
 		
