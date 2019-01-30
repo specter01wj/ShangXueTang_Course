680d0f589585742d7782fff6d85b7ad98e9fbe8f
specter01wj <specter01wj@gmail.com>
2019-01-30 01:58:21 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestTreeSet.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestTreeSet.java
@@ -1,10 +1,13 @@
 package com.accenture.spm.collection;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class TestTreeSet {
 
 	public static void main(String[] args) {
 		
-		
+		Set<Integer> set1 = new TreeSet<>();
 		
 	}
 	
