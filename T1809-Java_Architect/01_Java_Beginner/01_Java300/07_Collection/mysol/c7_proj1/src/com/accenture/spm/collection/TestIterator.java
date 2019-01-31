f71a98f5f4a15fd23f0efcc801d81f4e4366aecb
specter01wj <specter01wj@gmail.com>
2019-01-31 02:08:13 -0500

    01/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestIterator.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestIterator.java
@@ -1,19 +1,27 @@
 package com.accenture.spm.collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class TestIterator {
 
 	public static void main(String[] args) {
 		
-		
+		testIteratorList();
 		
 	}
 	
 	public static void testIteratorList() {
 		List<String> list1 = new ArrayList<>();
+		list1.add("aa1");
+		list1.add("bb1");
+		list1.add("cc1");
 		
+		for(Iterator<String> iter = list1.iterator(); iter.hasNext();) {
+			String temp = iter.next();
+			System.out.println(temp);
+		}
 		
 	}
 	
