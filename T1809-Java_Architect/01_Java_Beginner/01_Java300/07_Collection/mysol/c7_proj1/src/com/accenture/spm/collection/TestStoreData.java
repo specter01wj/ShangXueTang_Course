a34b7e044a9427be97f162e72754e22bca0ab588
specter01wj <specter01wj@gmail.com>
2019-02-01 01:17:51 -0500

    01/31/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestStoreData.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/07_Collection/mysol/c7_proj1/src/com/accenture/spm/collection/TestStoreData.java
@@ -13,6 +13,21 @@ public class TestStoreData {
 		row1.put("salary", 20000);
 		row1.put("date", "05/13/2015");
 		
+		Map<String, Object> row2 = new HashMap<>();
+		row2.put("id", 1002);
+		row2.put("name", "Kate");
+		row2.put("salary", 15000);
+		row2.put("date", "10/23/2016");
+		
+		Map<String, Object> row3 = new HashMap<>();
+		row3.put("id", 1003);
+		row3.put("name", "Kim");
+		row3.put("salary", 30000);
+		row3.put("date", "06/07/2008");
+		
+		
+		
+		
 	}
 	
 }
