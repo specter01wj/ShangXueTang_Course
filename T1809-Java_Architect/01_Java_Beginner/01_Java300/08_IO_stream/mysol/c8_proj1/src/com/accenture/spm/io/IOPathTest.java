a34e51bf0f73fd147d5e09190f67235091713613
specter01wj <specter01wj@gmail.com>
2019-02-02 12:48:05 -0500

    02/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOPathTest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOPathTest.java
@@ -1,11 +1,15 @@
 package com.accenture.spm.io;
 
+import java.io.File;
+
 public class IOPathTest {
 
 	public static void main(String[] args) {
 	
+		String path = "/Users/jinwang/Work/StudyGround/ShangXueTang/T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/IO.png";
+		System.out.println(File.separatorChar);
 		
-		
+		System.out.println(path);
 	}
 	
 }
