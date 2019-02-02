17bf66f74fc8f0edf621346616acdd56af29117d
specter01wj <specter01wj@gmail.com>
2019-02-02 13:00:02 -0500

    02/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFilePath.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFilePath.java
@@ -1,11 +1,14 @@
 package com.accenture.spm.io;
 
+import java.io.File;
+
 public class IOFilePath {
 
 	public static void main(String[] args) {
+		String path = "IO.png";
 		
-		
-		
+		File src = new File(path);
+		System.out.println(src.length());
 	}
 	
 }
