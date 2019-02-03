50c387c1cc0faf8f87fb8b4d7d3d4c87b9eea470
specter01wj <specter01wj@gmail.com>
2019-02-02 20:16:34 -0500

    02/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileLength.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileLength.java
@@ -7,7 +7,13 @@ public class IOFileLength {
 	public static void main(String[] args) {
 		File src = new File("IO.png");
 		
+		System.out.println("Len: " + src.length());
 		
+		src = new File("/Users/jinwang/Work/StudyGround/ShangXueTang/T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/IO.png");
+		System.out.println("Len Abs File: " + src.length());
+		
+		src = new File("/Users/jinwang/Work/StudyGround/ShangXueTang/T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/io_test");
+		System.out.println("Len Dir:"+ src.length());
 	}
 	
 }
