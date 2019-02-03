e97145e1de1d0b5c3845451bf476d040c2001ba6
specter01wj <specter01wj@gmail.com>
2019-02-03 11:58:17 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFilePrintList.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFilePrintList.java
@@ -1,10 +1,11 @@
 package com.accenture.spm.io;
 
+import java.io.File;
+
 public class IOFilePrintList {
 
 	public static void main(String[] args) {
-		
-		
+		File src =  new File("./");
 		
 	}
 	
