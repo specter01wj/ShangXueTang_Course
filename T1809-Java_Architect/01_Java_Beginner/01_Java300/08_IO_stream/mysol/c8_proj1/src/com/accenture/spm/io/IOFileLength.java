be55a6261dc82dc625237a313515a45f0478fe4b
specter01wj <specter01wj@gmail.com>
2019-02-02 20:13:03 -0500

    02/02/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileLength.java
@@ -0,0 +1,13 @@
+package com.accenture.spm.io;
+
+import java.io.File;
+
+public class IOFileLength {
+
+	public static void main(String[] args) {
+		File src = new File("IO.png");
+		
+		
+	}
+	
+}
