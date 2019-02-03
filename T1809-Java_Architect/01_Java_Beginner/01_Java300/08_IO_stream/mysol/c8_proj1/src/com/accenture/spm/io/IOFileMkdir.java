68722f2829137cfeafb723da1cf87377ed7b87bb
specter01wj <specter01wj@gmail.com>
2019-02-03 02:13:59 -0500

    02/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileMkdir.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileMkdir.java
@@ -1,10 +1,19 @@
 package com.accenture.spm.io;
 
+import java.io.File;
+
 public class IOFileMkdir {
 
 	public static void main(String[] args) {
+		File dir = new File("mkdir");
 		
+		boolean flag = dir.mkdir();
+		System.out.println(flag);
 		
+		File dir2 = new File("mkdirs/test");
+		
+		boolean flag2 = dir2.mkdirs();
+		System.out.println(flag2);
 		
 	}
 	
