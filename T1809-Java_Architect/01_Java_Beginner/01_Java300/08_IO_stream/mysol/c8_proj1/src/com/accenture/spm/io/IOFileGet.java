005aef59c9cb937ba650d9b049effb6f9fbbf66e
specter01wj <specter01wj@gmail.com>
2019-02-02 19:51:38 -0500

    02/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileGet.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileGet.java
@@ -5,10 +5,14 @@ import java.io.File;
 public class IOFileGet {
 
 	public static void main(String[] args) {
-		File src = new File("c8_proj1/IO.png");
+		File src = new File("IO.png");
 		
 		System.out.println(src.length());
 		System.out.println("name: " + src.getName());
+		System.out.println("path: " + src.getPath());
+		System.out.println("path: " + src.getAbsolutePath());
+		System.out.println("Parent path:" + src.getParent());
+		//System.out.println("Parent class:" + src.getParentFile().getName());
 	}
 	
 }
