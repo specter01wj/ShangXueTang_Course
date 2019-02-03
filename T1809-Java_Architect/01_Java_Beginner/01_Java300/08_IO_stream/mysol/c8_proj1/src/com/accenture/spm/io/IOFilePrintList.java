975029c8aac003f099fc57cd5597e897428427aa
specter01wj <specter01wj@gmail.com>
2019-02-03 12:01:17 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFilePrintList.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFilePrintList.java
@@ -7,6 +7,24 @@ public class IOFilePrintList {
 	public static void main(String[] args) {
 		File src =  new File("./");
 		
+		printName(src, 0);
+	}
+	
+	public static void printName(File src, int deep) {
+		
+		for(int i = 0; i < deep; i++) {
+			System.out.println("-");
+		}
+		
+		System.out.println(src.getName());
+		
+		if(null == src || !src.exists()) {
+			return;
+		} else if(src.isDirectory()) {
+			for(File s : src.listFiles()) {
+				printName(s, deep+1);
+			}
+		}
 	}
 	
 }
