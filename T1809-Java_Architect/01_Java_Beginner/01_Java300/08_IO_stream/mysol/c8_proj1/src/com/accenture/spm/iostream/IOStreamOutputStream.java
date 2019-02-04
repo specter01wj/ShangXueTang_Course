cdeabff8af2d05745d7c0fe0da0f0111228734b7
specter01wj <specter01wj@gmail.com>
2019-02-04 18:37:17 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamOutputStream.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamOutputStream.java
@@ -1,11 +1,38 @@
 package com.accenture.spm.iostream;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class IOStreamOutputStream {
 
 	public static void main(String[] args) {
 		
+		File src = new File("dest.txt");
+		OutputStream os = null;
 		
-		
+		try {
+			os = new FileOutputStream(src);
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(null!=os) {
+					os.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
