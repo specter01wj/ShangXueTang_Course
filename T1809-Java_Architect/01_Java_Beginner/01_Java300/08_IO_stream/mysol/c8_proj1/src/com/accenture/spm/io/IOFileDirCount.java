70d3ac7ee573f921bfa2fff006a99b3accdb530f
specter01wj <specter01wj@gmail.com>
2019-02-04 00:04:20 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileDirCount.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileDirCount.java
@@ -1,6 +1,19 @@
 package com.accenture.spm.io;
 
+import java.io.File;
+
 public class IOFileDirCount {
+
+	private long len;
+	private String path;
+	private File src;
+	
+	public IOFileDirCount(String path) {
+		this.path = path;
+		this.src = new File(path);
+	}
+
+
 
 	public static void main(String[] args) {
 		
