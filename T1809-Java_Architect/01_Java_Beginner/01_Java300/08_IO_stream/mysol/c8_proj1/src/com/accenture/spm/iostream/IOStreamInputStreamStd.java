3c8edcd2abb29c3acd619aa20fd781b2d8403421
specter01wj <specter01wj@gmail.com>
2019-02-04 16:50:25 -0500

    01/25/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamInputStreamStd.java
@@ -0,0 +1,33 @@
+package com.accenture.spm.iostream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IOStreamInputStreamStd {
+
+	public static void main(String[] args) {
+		
+		File src = new File("abc.txt");
+		
+		try {
+			InputStream is = new FileInputStream(src);
+			int temp;
+			while((temp = is.read()) != -1) {
+				System.out.println((char)temp);
+			}
+			
+			is.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
