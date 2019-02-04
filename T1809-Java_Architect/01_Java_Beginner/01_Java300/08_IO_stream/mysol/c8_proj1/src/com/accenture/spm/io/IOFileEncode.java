7cae82713776c64686fcc210ca03b14f9ef5750f
specter01wj <specter01wj@gmail.com>
2019-02-04 01:20:27 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileEncode.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileEncode.java
@@ -1,11 +1,20 @@
 package com.accenture.spm.io;
 
+import java.io.UnsupportedEncodingException;
+
 public class IOFileEncode {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String msg ="性命生命使命a";
 		
+		byte[] datas = msg.getBytes();
+		System.out.println(datas.length);
 		
+		datas = msg.getBytes("UTF-16LE");
+		System.out.println(datas.length);
 		
+		datas = msg.getBytes("GBK");
+		System.out.println(datas.length);	
 	}
 	
 }
