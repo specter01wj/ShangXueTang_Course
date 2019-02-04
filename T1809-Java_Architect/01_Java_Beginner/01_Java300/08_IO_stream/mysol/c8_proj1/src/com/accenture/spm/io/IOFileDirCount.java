7ba9b4c246069d42a7eb0b139138661216d3b82d
specter01wj <specter01wj@gmail.com>
2019-02-04 00:18:04 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileDirCount.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/io/IOFileDirCount.java
@@ -7,16 +7,33 @@ public class IOFileDirCount {
 	private long len;
 	private String path;
 	private File src;
+	private int fileSize;
+	private int dirSize;
 	
 	public IOFileDirCount(String path) {
 		this.path = path;
 		this.src = new File(path);
+		count(this.src);
 	}
 
+	private void count(File src) {	
+		
+		if(null!=src && src.exists()) {
+			if(src.isFile()) {
+				len+=src.length();
+				this.fileSize++;
+			}else {
+				this.dirSize++;
+				for(File s:src.listFiles()) {
+					count(s);
+				}
+			}
+		}
+	}	
 
 
 	public static void main(String[] args) {
-		
+		IOFileDirCount dir = new IOFileDirCount("./src");
 		
 		
 	}
