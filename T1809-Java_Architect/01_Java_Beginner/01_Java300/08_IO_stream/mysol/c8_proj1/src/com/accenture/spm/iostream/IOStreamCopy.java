eb2900515f1f31528964fe47db424ad178b214b2
specter01wj <specter01wj@gmail.com>
2019-02-05 17:17:34 -0500

    02/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamCopy.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamCopy.java
@@ -1,9 +1,17 @@
 package com.accenture.spm.iostream;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class IOStreamCopy {
 
 	public static void main(String[] args) {
+		File src = new File("abc.txt"); 
+		File dest = new File("io.txt");
 		
+		InputStream  is = null;
+		OutputStream os = null;
 		
 		
 	}
