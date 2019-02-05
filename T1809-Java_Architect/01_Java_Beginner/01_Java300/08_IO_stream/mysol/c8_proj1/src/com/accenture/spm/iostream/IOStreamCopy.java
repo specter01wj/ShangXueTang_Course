f4fc1492584a14179489db4bf1c7afaa655392cf
specter01wj <specter01wj@gmail.com>
2019-02-05 17:25:58 -0500

    02/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamCopy.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamCopy.java
@@ -1,6 +1,10 @@
 package com.accenture.spm.iostream;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -13,7 +17,38 @@ public class IOStreamCopy {
 		InputStream  is = null;
 		OutputStream os = null;
 		
-		
+		try {
+			is =new FileInputStream(src);
+			os = new FileOutputStream(dest);		
+			
+			byte[] flush = new byte[1024]; //缓冲容器
+			int len = -1; //接收长度
+			while((len=is.read(flush))!=-1) {
+				os.write(flush,0,len); //分段写出
+			}			
+			os.flush();
+		}catch(FileNotFoundException e) {		
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			//4、释放资源 分别关闭 先打开的后关闭
+			try {
+				if (null != os) {
+					os.close();
+				} 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				if(null!=is) {
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
