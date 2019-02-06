0645591136a8984f986be0bcc8fce56355ea55b3
specter01wj <specter01wj@gmail.com>
2019-02-06 18:22:06 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamPic.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamPic.java
@@ -1,11 +1,14 @@
 package com.accenture.spm.iostream;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IOStreamPic {
 
@@ -13,6 +16,7 @@ public class IOStreamPic {
 		
 		byte[] data = fileToByteArray("IO.png");
 		System.out.println(data.length);
+		ByteArrayToFile(data,"io-byte.png");	
 		
 	}
 	
@@ -53,7 +57,31 @@ public class IOStreamPic {
 	}
 	
 	public static void ByteArrayToFile(byte[] src, String filePath) {
-		
+		File dest = new File(filePath);
+		//2、选择流
+		InputStream  is =null;
+		OutputStream os =null;
+		try {
+			is =new ByteArrayInputStream(src);
+			os = new FileOutputStream(dest);
+			//3、操作 (分段读取)
+			byte[] flush = new byte[5]; //缓冲容器
+			int len = -1; //接收长度
+			while((len=is.read(flush))!=-1) {
+				os.write(flush,0,len);			//写出到文件	
+			}		
+			os.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			//4、释放资源
+			try {
+				if (null != os) {
+					os.close();
+				} 
+			} catch (Exception e) {
+			}
+		}
 	}
 	
 }
