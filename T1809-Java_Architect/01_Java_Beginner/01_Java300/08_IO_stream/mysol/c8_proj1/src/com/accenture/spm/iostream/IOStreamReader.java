2971ac041b14873cddb4473412efc01934621450
specter01wj <specter01wj@gmail.com>
2019-02-06 01:59:56 -0500

    02/05/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamReader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamReader.java
@@ -1,23 +1,23 @@
 package com.accenture.spm.iostream;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.Reader;
 
 public class IOStreamReader {
 
 public static void main(String[] args) {
 		
 		File src = new File("abc.txt");
-		InputStream is = null;
+		Reader reader = null;
 		try {
-			is = new FileInputStream(src);
+			reader = new FileReader(src);
 			
-			byte[] flush = new byte[5];
+			char[] flush = new char[1024];
 			int len = -1;
-			while((len = is.read(flush)) != -1) {
+			while((len = reader.read(flush)) != -1) {
 				String str = new String(flush, 0, len);
 				System.out.println(str);
 			}
@@ -31,8 +31,8 @@ public static void main(String[] args) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(null!=is) {
-					is.close();
+				if(null!=reader) {
+					reader.close();
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
