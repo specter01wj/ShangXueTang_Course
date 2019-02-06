3ad13dc2a32b2e3ad464a4fea4e41258d70524f9
specter01wj <specter01wj@gmail.com>
2019-02-06 18:20:09 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamPic.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamPic.java
@@ -1,15 +1,55 @@
 package com.accenture.spm.iostream;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class IOStreamPic {
 
 	public static void main(String[] args) {
 		
-		
+		byte[] data = fileToByteArray("IO.png");
+		System.out.println(data.length);
 		
 	}
 	
-	public static void fileToByteArray(String filePath) {
+	public static byte[] fileToByteArray(String filePath) {
+		File src = new File(filePath);
+		byte[] dest = null;
 		
+		InputStream is = null;
+		ByteArrayOutputStream baos =null;
+		try {
+			is = new FileInputStream(src);
+			baos = new ByteArrayOutputStream();
+			
+			byte[] flush = new byte[1024*10];
+			int len = -1;
+			while((len = is.read()) != -1) {
+				baos.write(flush,0,len);
+			}
+			baos.flush();
+			return baos.toByteArray();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(null!=is) {
+					is.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 	
 	public static void ByteArrayToFile(byte[] src, String filePath) {
