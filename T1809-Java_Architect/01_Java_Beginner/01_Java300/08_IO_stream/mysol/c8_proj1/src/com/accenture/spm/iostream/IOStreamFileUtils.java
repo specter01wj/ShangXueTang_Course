4b24002fb09f5c48cc9838136d4a5757af1efa3d
specter01wj <specter01wj@gmail.com>
2019-02-07 01:38:56 -0500

    02/03/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/iostream/IOStreamFileUtils.java
@@ -0,0 +1,56 @@
+package com.accenture.spm.iostream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class IOStreamFileUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			InputStream is = new FileInputStream("abc.txt");
+			OutputStream os = new FileOutputStream("abc_4.txt");
+			copy(is,os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void copy(InputStream is,OutputStream os) {
+		
+		try {
+			byte[] flush = new byte[1024];
+			int len = -1;
+			while((len=is.read(flush))!=-1) {
+				os.write(flush,0,len);
+			}			
+			os.flush();
+		}catch(FileNotFoundException e) {		
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if (null != os) {
+					os.close();
+				} 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				if(null!=is) {
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
