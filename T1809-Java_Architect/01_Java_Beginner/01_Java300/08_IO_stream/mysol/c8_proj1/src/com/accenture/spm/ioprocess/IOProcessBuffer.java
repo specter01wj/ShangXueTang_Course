4f430fb272ab143e137899bbbe61a4ec4bc0a841
specter01wj <specter01wj@gmail.com>
2019-02-08 23:56:55 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessBuffer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessBuffer.java
@@ -1,10 +1,43 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class IOProcessBuffer {
 
 	public static void main(String[] args) {
+		File src = new File("abc.txt");
+		//2、选择流
+		InputStream  is =null;
+		try {
+			is =new BufferedInputStream(new FileInputStream(src));
+			//3、操作 (分段读取)
+			byte[] flush = new byte[1024]; //缓冲容器
+			int len = -1; //接收长度
+			while((len=is.read(flush))!=-1) {
+				//字节数组-->字符串 (解码)
+				String str = new String(flush,0,len);
+				System.out.println(str);
+			}		
 		
-		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			//4、释放资源
+			try {
+				if(null!=is) {
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		}
 		
 	}
 	
