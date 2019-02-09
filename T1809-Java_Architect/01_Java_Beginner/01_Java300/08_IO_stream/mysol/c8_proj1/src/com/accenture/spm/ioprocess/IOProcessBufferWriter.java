7c9f3106ea2e7187b43d1dd4ebd15fb5a643061d
specter01wj <specter01wj@gmail.com>
2019-02-09 01:05:12 -0500

    01/25/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessBufferWriter.java
@@ -0,0 +1,34 @@
+package com.accenture.spm.ioprocess;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class IOProcessBufferWriter {
+
+	public static void main(String[] args) {
+		File dest = new File("dest.txt");
+		BufferedWriter writer =null;
+		try {
+			writer = new BufferedWriter(new FileWriter(dest));
+			writer.append("IO is so easy");
+			writer.newLine();
+			writer.append("尚学堂欢迎你");
+			writer.flush();
+		}catch(FileNotFoundException e) {		
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if (null != writer) {
+					writer.close();
+				} 
+			} catch (Exception e) {
+			}
+		}
+	}
+	
+}
