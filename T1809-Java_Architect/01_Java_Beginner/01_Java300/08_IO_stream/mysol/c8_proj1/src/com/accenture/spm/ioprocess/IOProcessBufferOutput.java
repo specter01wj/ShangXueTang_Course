f522a5591aaa946a289b4f7362e5229307d3ea5a
specter01wj <specter01wj@gmail.com>
2019-02-09 00:21:27 -0500

    01/25/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessBufferOutput.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessBufferOutput.java
@@ -1,10 +1,36 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class IOProcessBufferOutput {
 
 	public static void main(String[] args) {
 		
-		
+		File dest = new File("dest.txt");
+		OutputStream os =null;
+		try {
+			os =new BufferedOutputStream( new FileOutputStream(dest));
+			String msg ="IO is so easy\r\n";
+			byte[] datas =msg.getBytes();
+			os.write(datas,0,datas.length);
+			os.flush();
+		}catch(FileNotFoundException e) {		
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if (null != os) {
+					os.close();
+				} 
+			} catch (Exception e) {
+			}
+		}
 		
 	}
 	
