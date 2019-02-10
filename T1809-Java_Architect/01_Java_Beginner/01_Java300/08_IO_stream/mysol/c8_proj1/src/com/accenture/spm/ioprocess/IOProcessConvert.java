2d49c7540bba239e9df974fab184f876f8dc370c
specter01wj <specter01wj@gmail.com>
2019-02-09 20:44:43 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessConvert.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessConvert.java
@@ -1,10 +1,28 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class IOProcessConvert {
 
 	public static void main(String[] args) {
 		
-		
+		try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter writer =new BufferedWriter(new OutputStreamWriter(System.out));){
+					
+					String msg ="";
+					while(!msg.equals("exit")) {
+						msg = reader.readLine(); //循环读取
+						writer.write(msg); //循环写出
+						writer.newLine();
+						writer.flush(); //强制刷新
+					}
+				}catch(IOException e) {
+					System.out.println("操作异常");
+				}
 		
 	}
 	
