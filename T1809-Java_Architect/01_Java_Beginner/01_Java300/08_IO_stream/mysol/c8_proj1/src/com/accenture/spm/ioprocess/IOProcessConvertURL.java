72e953910451222cdcb487a8e9f22d72641e29a0
specter01wj <specter01wj@gmail.com>
2019-02-10 02:49:35 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessConvertURL.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessConvertURL.java
@@ -21,46 +21,46 @@ public class IOProcessConvertURL {
 						new BufferedWriter(
 								new OutputStreamWriter(
 										new FileOutputStream("baidu.html"),"UTF-8"));){
-			//3、操作 (读取)
+			
 			String msg ;
 			while((msg=reader.readLine())!=null) {
 				System.out.println(msg);
-				writer.write(msg); //字符集不统一不够出现乱码
+				writer.write(msg);
 				writer.newLine();
 			}					
 			writer.flush();
 		}catch(IOException e) {
-			System.out.println("操作异常");
+			System.out.println("Exception!");
 		}
 		
 	}
 	
 	public static void test2() {
-		//操作网络流  下载百度的源代码
+		
 		try(InputStreamReader is =
 				new InputStreamReader(new URL("http://www.baidu.com").openStream(),"UTF-8");){
-			//3、操作 (读取)
+			
 			int temp ;
 			while((temp=is.read())!=-1) {
 				System.out.print((char)temp);
 			}		
 			
 		}catch(IOException e) {
-			System.out.println("操作异常");
+			System.out.println("Exception!");
 		}
 	}
 	
 	public static void test1() {
-		//操作网络流  下载百度的源代码
+		
 		try(InputStream is =new URL("http://www.baidu.com").openStream();){
-			//3、操作 (读取)
+			
 			int temp ;
 			while((temp=is.read())!=-1) {
-				System.out.print((char)temp); //字节数不够出现乱码
+				System.out.print((char)temp);
 			}		
 			
 		}catch(IOException e) {
-			System.out.println("操作异常");
+			System.out.println("Exception!");
 		}
 		
 		
