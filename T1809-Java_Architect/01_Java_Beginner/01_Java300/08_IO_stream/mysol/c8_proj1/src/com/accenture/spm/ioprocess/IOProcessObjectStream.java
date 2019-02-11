c4b372470bb1cd53867e709d44d434f0c1c8d165
specter01wj <specter01wj@gmail.com>
2019-02-11 18:32:47 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectStream.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectStream.java
@@ -4,13 +4,14 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Date;
 
 public class IOProcessObjectStream {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
 		ByteArrayOutputStream baos =new ByteArrayOutputStream();
 		ObjectOutputStream oos =new ObjectOutputStream(new BufferedOutputStream(baos));
@@ -29,7 +30,28 @@ public class IOProcessObjectStream {
 		System.out.println(datas.length);
 		
 		ObjectInputStream ois =new ObjectInputStream(new BufferedInputStream(new ByteArrayInputStream(datas)));
+		String msg = ois.readUTF(); 
+		int age = ois.readInt();
+		boolean flag = ois.readBoolean();
+		char ch = ois.readChar();
+		System.out.println(flag);
+
+		Object str = ois.readObject();
+		Object date = ois.readObject();
+		Object employee = ois.readObject();
 		
+		if(str instanceof String) {
+			String strObj = (String) str;
+			System.out.println(strObj);
+		}
+		if(date instanceof Date) {
+			Date dateObj = (Date) date;
+			System.out.println(dateObj);
+		}
+		if(employee instanceof Employee) {
+			Employee empObj = (Employee) employee;
+			System.out.println(empObj.getName()+"-->"+empObj.getSalary());
+		}
 	}
 	
 }
