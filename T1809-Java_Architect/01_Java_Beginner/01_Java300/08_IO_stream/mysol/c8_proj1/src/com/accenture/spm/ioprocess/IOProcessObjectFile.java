ca0948b9948ee3474d49b440ff8b2022056c3e2a
specter01wj <specter01wj@gmail.com>
2019-02-11 18:52:00 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectFile.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectFile.java
@@ -12,29 +12,29 @@ import java.util.Date;
 public class IOProcessObjectFile {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		// 写出 -->序列化
+		
 		ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("obj.ser")));
-		// 操作数据类型 +数据
+		
 		oos.writeUTF("Accenture!");
 		oos.writeInt(18);
 		oos.writeBoolean(false);
 		oos.writeChar('a');
-		// 对象
+		
 		oos.writeObject("Federal Service?");
 		oos.writeObject(new Date());
 		Employee emp = new Employee("James", 400);
 		oos.writeObject(emp);
 		oos.flush();
 		oos.close();
-		// 读取 -->反序列化
+		
 		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream("obj.ser")));
-		// 顺序与写出一致
+		
 		String msg = ois.readUTF();
 		int age = ois.readInt();
 		boolean flag = ois.readBoolean();
 		char ch = ois.readChar();
 		System.out.println(flag);
-		// 对象的数据还原
+		
 		Object str = ois.readObject();
 		Object date = ois.readObject();
 		Object employee = ois.readObject();
