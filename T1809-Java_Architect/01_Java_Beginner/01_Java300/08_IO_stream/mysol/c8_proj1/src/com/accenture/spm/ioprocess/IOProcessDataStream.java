0dfc0a578b8755624f16cbbf99228db5f0f1c982
specter01wj <specter01wj@gmail.com>
2019-02-10 19:25:30 -0500

    02/10/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessDataStream.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessDataStream.java
@@ -1,5 +1,35 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class IOProcessDataStream {
 
+	public static void main(String[] args) throws IOException {
+		
+		ByteArrayOutputStream baos =new ByteArrayOutputStream();
+		DataOutputStream dos =new DataOutputStream(new BufferedOutputStream(baos));
+		
+		dos.writeUTF("编码辛酸泪");
+		dos.writeInt(18);
+		dos.writeBoolean(false);
+		dos.writeChar('a');
+		dos.flush();
+		byte[] datas =baos.toByteArray();
+		System.out.println(datas.length);
+		
+		DataInputStream dis =new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(datas)));
+		
+		String msg = dis.readUTF(); 
+		int age = dis.readInt();
+		boolean flag = dis.readBoolean();
+		char ch = dis.readChar();
+		System.out.println(flag);
+	}
+	
 }
