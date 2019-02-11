9e2dda0fc8cdcc7cee6eefbf40e3d519918eefd1
specter01wj <specter01wj@gmail.com>
2019-02-11 18:26:37 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectStream.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectStream.java
@@ -47,6 +47,22 @@ class Employee {
 		this.name = name;
 		this.salary = salary;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 	
 	
 	
