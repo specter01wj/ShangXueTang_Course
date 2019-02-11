1f6b0a0da6fb9e383fefda465d1cb8b7717f2d7a
specter01wj <specter01wj@gmail.com>
2019-02-11 18:25:24 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectStream.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessObjectStream.java
@@ -39,6 +39,12 @@ class Employee {
 	private String name;
 	private double salary;
 	
+	public Employee(String name, double salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+	
 	
 	
 }
