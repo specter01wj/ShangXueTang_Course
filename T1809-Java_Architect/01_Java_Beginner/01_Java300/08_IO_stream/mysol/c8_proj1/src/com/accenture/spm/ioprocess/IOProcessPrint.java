6d7545b6437b4901d73d20f645f145eeb0b61ce6
specter01wj <specter01wj@gmail.com>
2019-02-12 01:35:19 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessPrint.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessPrint.java
@@ -1,10 +1,14 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.PrintStream;
+
 public class IOProcessPrint {
 
 	public static void main(String[] args) {
 		
-		
+		PrintStream ps = System.out;
+		ps.println("Print Stream!");
+		ps.println(true);
 		
 	}
 	
