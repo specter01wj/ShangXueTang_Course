66c8a97e6558c5f4c569eda0d1c541a3c53a3658
specter01wj <specter01wj@gmail.com>
2019-02-12 01:37:25 -0500

    02/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessPrint.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessPrint.java
@@ -1,15 +1,22 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class IOProcessPrint {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		PrintStream ps = System.out;
 		ps.println("Print Stream!");
 		ps.println(true);
 		
+		ps = new PrintStream(new BufferedOutputStream(new FileOutputStream("print.txt")));
+		ps.println("Print Stream!");
+		ps.println(true);
+		ps.flush();
 	}
 	
 }
