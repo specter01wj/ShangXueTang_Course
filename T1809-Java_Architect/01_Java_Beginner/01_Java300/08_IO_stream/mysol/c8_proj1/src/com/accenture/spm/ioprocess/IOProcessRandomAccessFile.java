7b5a65a4ae6405eb477a987fe4b347f70b5e13dd
specter01wj <specter01wj@gmail.com>
2019-02-13 02:27:43 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessRandomAccessFile.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessRandomAccessFile.java
@@ -9,8 +9,29 @@ public class IOProcessRandomAccessFile {
 
 	public static void main(String[] args) throws IOException {
 		//test1();
-		test2();
+		//test2();
 		
+	}
+	
+	public static void split(int i,int beginPos,int actualSize ) throws IOException {
+		RandomAccessFile raf =new RandomAccessFile(new File("src/com/sxt/io/Copy.java"),"r");
+		//随机读取 
+		raf.seek(beginPos);
+		//读取
+		//3、操作 (分段读取)
+		byte[] flush = new byte[1024]; //缓冲容器
+		int len = -1; //接收长度
+		while((len=raf.read(flush))!=-1) {			
+			if(actualSize>len) { //获取本次读取的所有内容
+				System.out.println(new String(flush,0,len));
+				actualSize -=len;
+			}else { 
+				System.out.println(new String(flush,0,actualSize));
+				break;
+			}
+		}			
+		
+		raf.close();
 	}
 	
 	public static void test2() throws IOException {
