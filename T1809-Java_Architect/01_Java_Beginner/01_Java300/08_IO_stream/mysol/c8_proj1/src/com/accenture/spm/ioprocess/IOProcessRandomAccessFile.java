1092713f7fc9a5fd4fb2466b09c1be7bdfd52008
specter01wj <specter01wj@gmail.com>
2019-02-12 23:55:14 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessRandomAccessFile.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessRandomAccessFile.java
@@ -1,10 +1,14 @@
 package com.accenture.spm.ioprocess;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 public class IOProcessRandomAccessFile {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		
+		RandomAccessFile raf = new RandomAccessFile(new File("abc.txt"), "r");
 		
 	}
 	
