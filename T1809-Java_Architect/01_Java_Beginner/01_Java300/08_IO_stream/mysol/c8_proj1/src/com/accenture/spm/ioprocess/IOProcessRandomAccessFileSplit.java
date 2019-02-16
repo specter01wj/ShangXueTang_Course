f66d209857fdf5d6a5becfd790280b8cd1105b58
specter01wj <specter01wj@gmail.com>
2019-02-16 12:22:20 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessRandomAccessFileSplit.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/ioprocess/IOProcessRandomAccessFileSplit.java
@@ -65,24 +65,16 @@ public class IOProcessRandomAccessFileSplit {
 			splitDetail(i,beginPos,actualSize);
 		}
 	}	
-	/**
-	 * 指定第i块的起始位置 和实际长度
-	 * @param i
-	 * @param beginPos
-	 * @param actualSize
-	 * @throws IOException
-	 */
+	
 	private void splitDetail(int i,int beginPos,int actualSize ) throws IOException {
 		RandomAccessFile raf =new RandomAccessFile(this.src,"r");
 		RandomAccessFile raf2 =new RandomAccessFile(this.destPaths.get(i),"rw");
-		//随机读取 
+
 		raf.seek(beginPos);
-		//读取
-		//3、操作 (分段读取)
-		byte[] flush = new byte[1024]; //缓冲容器
-		int len = -1; //接收长度
+		byte[] flush = new byte[1024];
+		int len = -1;
 		while((len=raf.read(flush))!=-1) {			
-			if(actualSize>len) { //获取本次读取的所有内容
+			if(actualSize>len) {
 				raf2.write(flush, 0, len);
 				actualSize -=len;
 			}else { 
