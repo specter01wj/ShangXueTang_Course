f09501d2dbf01cc43986f45a5d9308e8c08f91f7
specter01wj <specter01wj@gmail.com>
2019-02-18 10:42:51 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/WebDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/WebDownloader.java
@@ -1,6 +1,8 @@
 package com.accenture.spm.thread;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
@@ -8,7 +10,17 @@ import org.apache.commons.io.FileUtils;
 public class WebDownloader {
 
 	public void download(String url, String name) {
-		FileUtils.copyURLToFile(new URL(url), new File(name));
+		try {
+			FileUtils.copyURLToFile(new URL(url), new File(name));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("illegal URL!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Download Fail!"); 
+		}
 	}
 	
 }
