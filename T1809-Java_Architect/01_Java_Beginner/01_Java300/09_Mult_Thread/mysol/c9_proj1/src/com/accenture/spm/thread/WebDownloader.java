b3282bfd01e789ae5c1ff6c36e500c7435cb8104
specter01wj <specter01wj@gmail.com>
2019-02-18 10:41:23 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/WebDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/WebDownloader.java
@@ -1,5 +1,10 @@
 package com.accenture.spm.thread;
 
+import java.io.File;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
 public class WebDownloader {
 
 	public void download(String url, String name) {
