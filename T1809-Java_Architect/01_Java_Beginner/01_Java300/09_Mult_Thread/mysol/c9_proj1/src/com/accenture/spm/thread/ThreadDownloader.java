0884ba885da8194040168ecdaf8609e0da2703fd
specter01wj <specter01wj@gmail.com>
2019-02-18 11:01:25 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
@@ -19,7 +19,7 @@ public class ThreadDownloader extends Thread {
 	public static void main(String[] args) {
 		ThreadDownloader td1 = new ThreadDownloader("https://tfwiki.net/wiki/File:Masterplan2_earth_germ.jpg", "tf.jpg");
 		ThreadDownloader td2 = new ThreadDownloader("http://p1.pstatp.com/large/403c00037462ae2eee13","spl.jpg");
-		ThreadDownloader td3 = new ThreadDownloader("http://5b0988e595225.cdn.sohucs.com/images/20170830/d8b57e0dce0d4fa29bd5ef014be663d5.jpeg","success.jpg");
+		ThreadDownloader td3 = new ThreadDownloader("http://5b0988e595225.cdn.sohucs.com/images/20170830/d8b57e0dce0d4fa29bd5ef014be663d5.jpeg","brucelee.jpg");
 
 		td1.start();
 		td2.start();
