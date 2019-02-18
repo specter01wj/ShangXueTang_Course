9871d7e7fa3a0883dd96b5e710fe6978181ff95f
specter01wj <specter01wj@gmail.com>
2019-02-18 10:49:50 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
@@ -5,8 +5,11 @@ public class ThreadDownloader extends Thread {
 	private String url;
 	private String name;
 	
-	
-	
+	public ThreadDownloader(String url, String name) {
+		this.url = url;
+		this.name = name;
+	}
+
 	@Override
 	public void run() {
 		
