7525d67e4a70072df986221a5aafed36c781d03e
specter01wj <specter01wj@gmail.com>
2019-02-18 10:49:20 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
@@ -1,6 +1,16 @@
 package com.accenture.spm.thread;
 
-public class ThreadDownloader {
+public class ThreadDownloader extends Thread {
+
+	private String url;
+	private String name;
+	
+	
+	
+	@Override
+	public void run() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
