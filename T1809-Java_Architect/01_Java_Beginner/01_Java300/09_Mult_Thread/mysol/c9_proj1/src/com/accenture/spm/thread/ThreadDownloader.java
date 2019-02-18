3674e11d1135d25b16d647d980833f9c270d5c64
specter01wj <specter01wj@gmail.com>
2019-02-18 11:01:05 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadDownloader.java
@@ -17,8 +17,13 @@ public class ThreadDownloader extends Thread {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ThreadDownloader td1 = new ThreadDownloader("https://tfwiki.net/wiki/File:Masterplan2_earth_germ.jpg", "tf.jpg");
+		ThreadDownloader td2 = new ThreadDownloader("http://p1.pstatp.com/large/403c00037462ae2eee13","spl.jpg");
+		ThreadDownloader td3 = new ThreadDownloader("http://5b0988e595225.cdn.sohucs.com/images/20170830/d8b57e0dce0d4fa29bd5ef014be663d5.jpeg","success.jpg");
 
+		td1.start();
+		td2.start();
+		td3.start();
 	}
 
 }
