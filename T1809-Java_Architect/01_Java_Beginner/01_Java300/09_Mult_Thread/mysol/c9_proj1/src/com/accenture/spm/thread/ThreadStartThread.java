6401dcd871f79cfa5c6e28dc0fb6da51482609aa
specter01wj <specter01wj@gmail.com>
2019-02-17 22:14:46 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadStartThread.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/ThreadStartThread.java
@@ -4,7 +4,9 @@ public class ThreadStartThread extends Thread {
 
 	@Override
 	public void run() {
-		
+		for(int i=0;i<20;i++) {
+			System.out.println("Sing!");
+		}
 	}
 
 	public static void main(String[] args) {
