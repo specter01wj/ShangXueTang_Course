24e880f29b9e60d5ced3365c66c53b58c3cc51ea
specter01wj <specter01wj@gmail.com>
2019-02-19 00:16:39 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableWeb12306.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableWeb12306.java
@@ -1,10 +1,31 @@
 package com.accenture.spm.runnable;
 
-public class RunnableWeb12306 {
+public class RunnableWeb12306 implements Runnable {
 
+	private int ticketNums = 99;
+	
+	@Override
+	public void run() {
+		while(true) {
+			if(ticketNums<0) {
+				break;
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+"-->"+ticketNums--);
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//一份资源
+		RunnableWeb12306 web =new RunnableWeb12306();
+		System.out.println(Thread.currentThread().getName());
+		//多个代理
+		new Thread(web,"James").start();
+		new Thread(web,"Kim").start();
+		new Thread(web,"Lee").start();;
 	}
 
 }
