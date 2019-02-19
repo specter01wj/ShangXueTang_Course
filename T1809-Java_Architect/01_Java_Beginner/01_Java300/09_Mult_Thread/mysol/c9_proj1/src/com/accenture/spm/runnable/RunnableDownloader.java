bb359649af2f8cf16fb31c3d03c82e6ca196ba1e
specter01wj <specter01wj@gmail.com>
2019-02-19 00:09:16 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableDownloader.java
@@ -2,8 +2,8 @@ package com.accenture.spm.runnable;
 import com.accenture.spm.thread.WebDownloader;
 
 public class RunnableDownloader implements Runnable {
-	private String url; //远程路径
-	private String name;  //存储名字
+	private String url;
+	private String name;
 	
 	public RunnableDownloader(String url, String name) {
 		this.url = url; 
@@ -22,7 +22,6 @@ public class RunnableDownloader implements Runnable {
 		RunnableDownloader td2 =new RunnableDownloader("http://p1.pstatp.com/large/403c00037462ae2eee13","spl_2.jpg");
 		RunnableDownloader td3 =new RunnableDownloader("http://5b0988e595225.cdn.sohucs.com/images/20170830/d8b57e0dce0d4fa29bd5ef014be663d5.jpeg","brucelee_2.jpg");
 		
-		//启动三个线程
 		new Thread(td1).start();
 		new Thread(td2).start();
 		new Thread(td3).start();
