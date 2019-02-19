03131c6f6e641187b64f5df567c87638ab4f672d
specter01wj <specter01wj@gmail.com>
2019-02-18 23:58:39 -0700

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableStartRun.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableStartRun.java
@@ -1,6 +1,14 @@
 package com.accenture.spm.runnable;
 
-public class RunnableStartRun {
+public class RunnableStartRun implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=0;i<20;i++) {
+			System.out.println("Sing!!!");
+		}
+	}
 
 	public static void main(String[] args) {
 		
