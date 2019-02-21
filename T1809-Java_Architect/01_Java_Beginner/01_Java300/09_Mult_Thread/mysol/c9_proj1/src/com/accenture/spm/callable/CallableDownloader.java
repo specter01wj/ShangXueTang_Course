287600537e6cd302bed8861dbc460638a1af7f45
specter01wj <specter01wj@gmail.com>
2019-02-21 00:35:12 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
@@ -1,9 +1,12 @@
 package com.accenture.spm.callable;
 
-import com.accenture.spm.thread.WebDownloader;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class CallableDownloader implements Callable {
+import com.accenture.spm.thread.WebDownloader;
+
+public class CallableDownloader implements Callable<Boolean> {
 	private String url;
 	private String name;
 	
@@ -13,21 +16,21 @@ public class CallableDownloader implements Callable {
 	}
 
 	@Override
-	public Object call() throws Exception {
+	public Boolean call() throws Exception {
 		WebDownloader wd =new WebDownloader();
 		wd.download(url, name);		
 		System.out.println(name);
 		return true;
 	}
 
-
-
 	public static void main(String[] args) {
 		CallableDownloader cd1 =new CallableDownloader("http://images6.fanpop.com/image/photos/33800000/Decepticon-Skystalker-transformers-33827443-988-508.jpg","tf_3.jpg");
 		CallableDownloader cd2 =new CallableDownloader("http://p1.pstatp.com/large/403c00037462ae2eee13","spl_3.jpg");
 		CallableDownloader cd3 =new CallableDownloader("http://5b0988e595225.cdn.sohucs.com/images/20170830/d8b57e0dce0d4fa29bd5ef014be663d5.jpeg","brucelee_3.jpg");
 		
-
+		ExecutorService  ser=Executors.newFixedThreadPool(3);
+		
+		
 	}
 
 }
