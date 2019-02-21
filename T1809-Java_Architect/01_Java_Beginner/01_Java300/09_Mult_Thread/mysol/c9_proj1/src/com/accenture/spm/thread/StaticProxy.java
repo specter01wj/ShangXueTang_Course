0fc658340c071a9b48040df77130240b597ff1a6
specter01wj <specter01wj@gmail.com>
2019-02-21 17:10:31 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/StaticProxy.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/thread/StaticProxy.java
@@ -4,8 +4,47 @@ public class StaticProxy {
 
 	public static void main(String[] args) {
 		
-		
-		
+		new WeddingCompany(new You()).happyMarry();
+		//new Thread().start();
+	}
+	
+}
+
+interface Marry {
+	void happyMarry();
+}
+
+class You implements Marry {
+
+	@Override
+	public void happyMarry() {
+		// TODO Auto-generated method stub
+		System.out.println("James Marry!!!");
+	}
+	
+}
+
+class WeddingCompany implements Marry {
+	private Marry target;
+
+	public WeddingCompany(Marry target) {
+		this.target = target;
+	}
+
+	@Override
+	public void happyMarry() {
+		// TODO Auto-generated method stub
+		ready();
+		this.target.happyMarry();
+		after();
+	}
+	
+	private void ready() {
+		System.out.println("Ready....");
+	}
+	
+	private void after() {
+		System.out.println("After-----");
 	}
 	
 }
