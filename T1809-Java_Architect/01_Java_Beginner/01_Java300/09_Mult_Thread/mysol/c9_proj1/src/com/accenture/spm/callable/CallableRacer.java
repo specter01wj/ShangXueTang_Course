a11aae7f612cc978f4a892f68c38354afcd09e47
specter01wj <specter01wj@gmail.com>
2019-02-21 01:40:16 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableRacer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableRacer.java
@@ -1,7 +1,40 @@
 package com.accenture.spm.callable;
 
-public class CallableRacer {
+import java.util.concurrent.Callable;
 
+public class CallableRacer implements Callable<Integer> {
+	private  String winner;
+	
+	@Override
+	public Integer call() throws Exception {
+		for(int steps =1;steps<=100;steps++) {		
+			//模拟休息
+			if(Thread.currentThread().getName().equals("pool-1-thread-1") && steps%10==0) {
+				Thread.sleep(100);
+			}
+			System.out.println(Thread.currentThread().getName()+"-->"+steps);
+			//比赛是否结束
+			boolean flag = gameOver(steps);
+			if(flag) {
+				return steps;
+			}
+		}
+		return null;
+	}
+	
+	private boolean gameOver(int steps) {
+		if(winner!=null) { //存在胜利者
+			return true;
+		}else {
+			if(steps ==100) {
+				winner = Thread.currentThread().getName();
+				System.out.println("winner==>"+winner);
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
