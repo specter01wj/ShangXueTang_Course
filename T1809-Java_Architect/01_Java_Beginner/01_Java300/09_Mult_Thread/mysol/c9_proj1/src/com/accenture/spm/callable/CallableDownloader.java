43738acca5422088cbf738aa29875b3b234e8d1e
specter01wj <specter01wj@gmail.com>
2019-02-20 23:50:18 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
@@ -1,7 +1,13 @@
 package com.accenture.spm.callable;
 
-public class CallableDownloader {
+import java.util.concurrent.Callable;
 
+public class CallableDownloader implements Callable {
+	private String url;
+	private String name;
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
