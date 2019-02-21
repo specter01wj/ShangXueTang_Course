2c65b02a88ef464f8d7fcb5b41bbac60fddd1597
specter01wj <specter01wj@gmail.com>
2019-02-21 00:37:53 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
@@ -1,6 +1,7 @@
 package com.accenture.spm.callable;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -24,7 +25,7 @@ public class CallableDownloader implements Callable<Boolean> {
 		return true;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		CallableDownloader cd1 =new CallableDownloader("http://images6.fanpop.com/image/photos/33800000/Decepticon-Skystalker-transformers-33827443-988-508.jpg","tf_3.jpg");
 		CallableDownloader cd2 =new CallableDownloader("http://p1.pstatp.com/large/403c00037462ae2eee13","spl_3.jpg");
 		CallableDownloader cd3 =new CallableDownloader("http://5b0988e595225.cdn.sohucs.com/images/20170830/d8b57e0dce0d4fa29bd5ef014be663d5.jpeg","brucelee_3.jpg");
@@ -34,6 +35,13 @@ public class CallableDownloader implements Callable<Boolean> {
 		Future<Boolean> result1 =ser.submit(cd1);
 		Future<Boolean> result2 =ser.submit(cd2);
 		Future<Boolean> result3 =ser.submit(cd3);
+		
+		boolean r1 =result1.get();
+		boolean r2 =result1.get();
+		boolean r3 =result1.get();
+		System.out.println(r3);
+		
+		ser.shutdownNow();
 	}
 
 }
