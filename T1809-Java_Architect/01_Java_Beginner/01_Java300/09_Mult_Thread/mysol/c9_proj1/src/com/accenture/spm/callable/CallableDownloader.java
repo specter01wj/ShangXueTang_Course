9c11e11c1270ec1dacc3c9694a696fedfbdda90a
specter01wj <specter01wj@gmail.com>
2019-02-20 23:55:29 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/callable/CallableDownloader.java
@@ -6,8 +6,13 @@ public class CallableDownloader implements Callable {
 	private String url;
 	private String name;
 	
-	
-	
+	public CallableDownloader(String url, String name) {
+		this.url = url;
+		this.name = name;
+	}
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
