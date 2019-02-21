4f47add31b20dba1193c63c491a614332fc6e1ca
specter01wj <specter01wj@gmail.com>
2019-02-20 20:03:11 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableRacer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/runnable/RunnableRacer.java
@@ -1,21 +1,21 @@
 package com.accenture.spm.runnable;
 
 public class RunnableRacer implements Runnable {
-
-	private  String winner;//胜利者
+	private  String winner;
+	
 	@Override
 	public void run() {
 		for(int steps =1;steps<=100;steps++) {		
-			//模拟休息
-			if(Thread.currentThread().getName().equals("rabbit") && steps%10==0) {
+			
+			/*if(Thread.currentThread().getName().equals("rabbit") && steps%10==0) {
 				try {
 					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			}
+			}*/
 			System.out.println(Thread.currentThread().getName()+"-->"+steps);
-			//比赛是否结束
+			
 			boolean flag = gameOver(steps);
 			if(flag) {
 				break;
@@ -23,7 +23,7 @@ public class RunnableRacer implements Runnable {
 		}
 	}
 	private boolean gameOver(int steps) {
-		if(winner!=null) { //存在胜利者
+		if(winner!=null) {
 			return true;
 		}else {
 			if(steps ==100) {
