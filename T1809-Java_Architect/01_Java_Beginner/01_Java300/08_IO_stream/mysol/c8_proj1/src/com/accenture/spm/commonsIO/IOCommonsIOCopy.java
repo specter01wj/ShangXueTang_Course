f22b59cbc2dedcc584f8042d8f4822d7984e0739
specter01wj <specter01wj@gmail.com>
2019-02-22 02:47:39 -0500

    02/21/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOCopy.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOCopy.java
@@ -1,9 +1,28 @@
 package com.accenture.spm.commonsIO;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 public class IOCommonsIOCopy {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		FileUtils.copyFile(new File("p.png"),new File("p-copy.png"));
+				//复制文件到目录
+				//FileUtils.copyFileToDirectory(new File("p.png"),new File("lib"));
+				//复制目录到目录
+				//FileUtils.copyDirectoryToDirectory(new File("lib"),new File("lib2"));
+				//复制目录
+				//FileUtils.copyDirectory(new File("lib"),new File("lib2"));
+				//拷贝URL内容
+				//String url = "https://pic2.zhimg.com/v2-7d01cab20858648cbf62333a7988e6d0_qhd.jpg";
+				//FileUtils.copyURLToFile(new URL(url), new File("marvel.jpg"));
+				String datas =IOUtils.toString(new URL("http://www.163.com"), "gbk");
+				System.out.println(datas);
 
 	}
 
