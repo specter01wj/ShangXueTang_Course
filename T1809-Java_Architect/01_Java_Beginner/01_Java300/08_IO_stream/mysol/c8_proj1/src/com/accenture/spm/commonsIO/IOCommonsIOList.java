f805560b7b07db02220982b3b8ef414655413a0a
specter01wj <specter01wj@gmail.com>
2019-02-22 02:20:55 -0500

    02/21/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOList.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOList.java
@@ -12,25 +12,32 @@ import org.apache.commons.io.filefilter.SuffixFileFilter;
 public class IOCommonsIOList {
 
 	public static void main(String[] args) {
-		Collection<File> files =FileUtils.listFiles(new File("D:\\java300\\IO_study04"),
+		
+		Collection<File> files =FileUtils.listFiles(new File("lib"),
 				EmptyFileFilter.NOT_EMPTY, null);
 		for (File file : files) {
 			System.out.println(file.getAbsolutePath());
 		}
+		
 		System.out.println("---------------------");
-		 files =FileUtils.listFiles(new File("D:\\java300\\IO_study04"),
+		
+		 files =FileUtils.listFiles(new File("lib"),
 					EmptyFileFilter.NOT_EMPTY, DirectoryFileFilter.INSTANCE);
 		for (File file : files) {
 			System.out.println(file.getAbsolutePath());
 		}
+		
 		System.out.println("---------------------");
-		 files =FileUtils.listFiles(new File("D:\\java300\\IO_study04"),
+		
+		 files =FileUtils.listFiles(new File("lib"),
 					new SuffixFileFilter("java"), DirectoryFileFilter.INSTANCE);
 		for (File file : files) {
 			System.out.println(file.getAbsolutePath());
 		}
+		
 		System.out.println("---------------------");
-		 files =FileUtils.listFiles(new File("D:\\java300\\IO_study04"),
+		
+		 files =FileUtils.listFiles(new File("lib"),
 					FileFilterUtils.or(new SuffixFileFilter("java"),
 							new SuffixFileFilter("class"),EmptyFileFilter.EMPTY), DirectoryFileFilter.INSTANCE);
 		for (File file : files) {
@@ -38,7 +45,8 @@ public class IOCommonsIOList {
 		}
 		
 		System.out.println("---------------------");
-		 files =FileUtils.listFiles(new File("D:\\java300\\IO_study04"),
+		
+		 files =FileUtils.listFiles(new File("lib"),
 					FileFilterUtils.and(new SuffixFileFilter("java"),
 							EmptyFileFilter.NOT_EMPTY), DirectoryFileFilter.INSTANCE);
 		for (File file : files) {
