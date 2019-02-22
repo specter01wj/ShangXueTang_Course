72b8add2154c7054e259637a66974c5a3adbc478
specter01wj <specter01wj@gmail.com>
2019-02-22 02:50:11 -0500

    02/21/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOCopy.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOCopy.java
@@ -11,18 +11,18 @@ public class IOCommonsIOCopy {
 
 	public static void main(String[] args) throws IOException {
 		
-		FileUtils.copyFile(new File("p.png"),new File("p-copy.png"));
-				//复制文件到目录
-				//FileUtils.copyFileToDirectory(new File("p.png"),new File("lib"));
-				//复制目录到目录
-				//FileUtils.copyDirectoryToDirectory(new File("lib"),new File("lib2"));
-				//复制目录
-				//FileUtils.copyDirectory(new File("lib"),new File("lib2"));
-				//拷贝URL内容
-				//String url = "https://pic2.zhimg.com/v2-7d01cab20858648cbf62333a7988e6d0_qhd.jpg";
-				//FileUtils.copyURLToFile(new URL(url), new File("marvel.jpg"));
-				String datas =IOUtils.toString(new URL("http://www.163.com"), "gbk");
-				System.out.println(datas);
+		FileUtils.copyFile(new File("lib/p.png"),new File("lib/p-copy.png"));
+		
+		FileUtils.copyFileToDirectory(new File("abc.txt"),new File("lib"));
+		
+		FileUtils.copyDirectoryToDirectory(new File("lib"),new File("lib2"));
+		
+		FileUtils.copyDirectory(new File("lib2"),new File("lib3"));
+		
+		String url = "https://pic2.zhimg.com/v2-7d01cab20858648cbf62333a7988e6d0_qhd.jpg";
+		FileUtils.copyURLToFile(new URL(url), new File("marvel.jpg"));
+		String datas =IOUtils.toString(new URL("http://www.163.com"), "gbk");
+		System.out.println(datas);
 
 	}
 
