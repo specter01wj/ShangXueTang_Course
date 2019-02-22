2d8874fdddadda902c0c6f6d977d51897034ab71
specter01wj <specter01wj@gmail.com>
2019-02-22 18:07:18 -0500

    02/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaThread.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaThread.java
@@ -2,8 +2,16 @@ package com.accenture.spm.lambda;
 
 public class LambdaThread {
 
+	static class Test implements Runnable{		
+		public void run() {
+			for(int i=0;i<20;i++) {
+				System.out.println("Singing!!!");
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Thread(new Test()).start();
 
 	}
 
