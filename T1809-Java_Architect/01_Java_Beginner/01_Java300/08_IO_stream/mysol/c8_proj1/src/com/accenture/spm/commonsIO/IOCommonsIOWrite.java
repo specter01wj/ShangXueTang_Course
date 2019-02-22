0a6edf487a19c95064d6581f12521bd6025a28f3
specter01wj <specter01wj@gmail.com>
2019-02-22 02:41:14 -0500

    02/21/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOWrite.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/08_IO_stream/mysol/c8_proj1/src/com/accenture/spm/commonsIO/IOCommonsIOWrite.java
@@ -3,7 +3,17 @@ package com.accenture.spm.commonsIO;
 public class IOCommonsIOWrite {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		FileUtils.write(new File("happy.sxt"), "The father of a so-called\r\n","UTF-8");
+		FileUtils.writeStringToFile(new File("happy.sxt"), "who was born and raised in the U.S.\r\n","UTF-8",true);
+		FileUtils.writeByteArrayToFile(new File("happy.sxt"), "and now wants to return home with her son\r\n".getBytes("UTF-8"),true);
+		
+		List<String> datas =new ArrayList<String>();
+		datas.add("James");
+		datas.add("Kim");
+		datas.add("George");
+		
+		FileUtils.writeLines(new File("happy.sxt"), datas,"。。。。。",true);
 
 	}
 
