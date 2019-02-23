d33a72f5153b60594d6b8d5fd415bddf00fd712c
specter01wj <specter01wj@gmail.com>
2019-02-23 01:57:36 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaTest1.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaTest1.java
@@ -6,6 +6,19 @@ public class LambdaTest1 {
 		// TODO Auto-generated method stub
 		ILike like = new Like();
 		like.lambda();
+		
+		ILike like2 = new Like2();
+		like2.lambda();
+	}
+	
+	static class Like2 implements ILike {
+
+		@Override
+		public void lambda() {
+			// TODO Auto-generated method stub
+			System.out.println("James - Lambda (in)!");
+		}
+		
 	}
 
 }
