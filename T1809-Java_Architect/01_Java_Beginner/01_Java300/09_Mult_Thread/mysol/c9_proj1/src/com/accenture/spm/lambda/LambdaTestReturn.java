873adb14a69f2f599064318099a8557ad3e04453
specter01wj <specter01wj@gmail.com>
2019-02-23 02:18:53 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaTestReturn.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaTestReturn.java
@@ -4,7 +4,28 @@ public class LambdaTestReturn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		IInterest interest = (int a,int c)-> {
+			System.out.println("i like lambda -->"+(a+c));
+			return a+c;
+		};
+		interest.lambda(100,200);
+		
+		interest = (a,c)-> {
+			System.out.println("i like lambda -->"+(a+c));
+			return a+c;
+		};
+		
+		interest.lambda(200,200);
+		
+		interest = (a,c)-> {
+			return a+c;
+		};
+		
+		//interest = (a,c)-> a+c;
+		
+		//interest = (a,c)-> 100;
+		
+		System.out.println(interest.lambda(10, 20));
 	}
 
 }
