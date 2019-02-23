8c4f02be22a1e4adbbe0b7b507218776cabc13d4
specter01wj <specter01wj@gmail.com>
2019-02-23 16:12:33 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateTerminateThread.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateTerminateThread.java
@@ -1,6 +1,25 @@
 package com.accenture.spm.state;
 
-public class StateTerminateThread {
+public class StateTerminateThread implements Runnable {
+
+	private boolean flag = true;
+	private String name;
+	
+	public StateTerminateThread(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		int i=0;
+		
+		while(flag) {
+			System.out.println(name + "-->" + i++);
+		}
+		
+	}
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
