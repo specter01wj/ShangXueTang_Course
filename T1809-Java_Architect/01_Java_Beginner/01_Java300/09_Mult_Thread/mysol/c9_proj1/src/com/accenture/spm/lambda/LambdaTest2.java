ef643cd66105194feb7dcbe11b3a171f7ad90adf
specter01wj <specter01wj@gmail.com>
2019-02-23 02:07:38 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaTest2.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaTest2.java
@@ -4,7 +4,14 @@ public class LambdaTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ILove love =(int a) -> {
+			System.out.println("i like lambda -->"+a);
+		};
+		
+		love.lambda(100);
+		
+		
+		
 	}
 
 }
