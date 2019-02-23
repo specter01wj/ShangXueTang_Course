1284db4bd5e3593af4015e72137dd0ec9bfc9333
specter01wj <specter01wj@gmail.com>
2019-02-23 02:21:43 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaThreadSimp.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/lambda/LambdaThreadSimp.java
@@ -4,7 +4,13 @@ public class LambdaThreadSimp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Thread(()->{
+			for(int i=0;i<10;i++) {
+				System.out.println("James - Lambda!");
+			}
+		}) .start();
+		
+		new Thread(()->	System.out.println("Alica = Lambda2?")) .start();
 	}
 
 }
