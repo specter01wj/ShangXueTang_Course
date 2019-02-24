c8f4c232ab658bfcbf7895f421cc94029b798a0d
specter01wj <specter01wj@gmail.com>
2019-02-24 01:46:10 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepCountdown.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepCountdown.java
@@ -1,10 +1,30 @@
 package com.accenture.spm.state;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class StateBlockedSleepCountdown {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		Date endTime=new Date(System.currentTimeMillis()+1000*10);
+		long end = endTime.getTime();
+		while(true) {
+			System.out.println(new SimpleDateFormat("mm:ss").format(endTime));
+			Thread.sleep(1000);
+			 endTime=new Date(endTime.getTime()-1000);		
+			 if(end-10000 >endTime.getTime() ) {
+				 break;
+			 }
+		}
+	}
+	
+	public static void test() throws InterruptedException {
+		//倒数10个数，1秒一个
+		int num = 10;
+		while(true) {
+			Thread.sleep(1000);
+			System.out.println(num--);
+		}
 	}
 
 }
