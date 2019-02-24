5587b1246c433c664ba7626562af6fdf02c5fd88
specter01wj <specter01wj@gmail.com>
2019-02-24 03:12:23 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedJoin.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedJoin.java
@@ -2,9 +2,20 @@ package com.accenture.spm.state;
 
 public class StateBlockedJoin {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		Thread t=new Thread(()->{
+			for(int i=0;i<100;i++) {
+					System.out.println("lambda...."+i);
+			}
+		});
+		t.start();
+		
+		for(int i=0;i<100;i++) {
+			if(i==20) {
+				t.join();
+			}
+			System.out.println("main...."+i);
+		}
 	}
 
 }
