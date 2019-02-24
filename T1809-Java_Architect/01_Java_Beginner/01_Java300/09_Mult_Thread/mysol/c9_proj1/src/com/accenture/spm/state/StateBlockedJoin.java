d591ca2927b711ca42037269ea3e74722ee83815
specter01wj <specter01wj@gmail.com>
2019-02-24 03:15:52 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedJoin.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedJoin.java
@@ -4,14 +4,14 @@ public class StateBlockedJoin {
 
 	public static void main(String[] args) throws InterruptedException {
 		Thread t=new Thread(()->{
-			for(int i=0;i<100;i++) {
+			for(int i=0;i<40;i++) {
 					System.out.println("lambda...."+i);
 			}
 		});
 		t.start();
 		
-		for(int i=0;i<100;i++) {
-			if(i==20) {
+		for(int i=0;i<10;i++) {
+			if(i==5) {
 				t.join();
 			}
 			System.out.println("main...."+i);
