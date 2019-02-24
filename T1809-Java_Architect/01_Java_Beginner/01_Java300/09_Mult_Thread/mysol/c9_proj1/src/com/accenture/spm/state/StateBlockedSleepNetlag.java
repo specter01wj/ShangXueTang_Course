bf9f007b108f0ecddcfb1bd916caf57691194fb9
specter01wj <specter01wj@gmail.com>
2019-02-23 23:59:37 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepNetlag.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepNetlag.java
@@ -8,3 +8,24 @@ public class StateBlockedSleepNetlag {
 	}
 
 }
+
+class Web12306 implements Runnable {
+	
+	private int ticketNums = 99;
+	
+	@Override
+	public void run() {
+		while(true) {
+			if(ticketNums<0) {
+				break;
+			}
+			
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+"-->"+ticketNums--);
+		}
+	}	
+}
