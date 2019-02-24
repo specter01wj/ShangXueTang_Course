92b66d4664bfce4e6d527e973265a3ca269c4fe5
specter01wj <specter01wj@gmail.com>
2019-02-24 03:07:05 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepYieldLambda.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepYieldLambda.java
@@ -3,8 +3,18 @@ package com.accenture.spm.state;
 public class StateBlockedSleepYieldLambda {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Thread(()->{
+			for(int i=0;i<40;i++) {
+				System.out.println("lambda...."+i);
+			}
+		}) .start();
+		
+		for(int i=0;i<20;i++) {
+			if(i%3==0) {
+				Thread.yield();
+			}
+			System.out.println("main...."+i);
+		}
 	}
 
 }
