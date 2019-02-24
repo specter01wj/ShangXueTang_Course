7b4406e493d806fe5fd5da7e70084cf20682507d
specter01wj <specter01wj@gmail.com>
2019-02-24 01:39:33 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepSimsleep.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/09_Mult_Thread/mysol/c9_proj1/src/com/accenture/spm/state/StateBlockedSleepSimsleep.java
@@ -8,3 +8,40 @@ public class StateBlockedSleepSimsleep {
 	}
 
 }
+
+class Racer implements Runnable {
+	private  String winner;//胜利者
+	@Override
+	public void run() {
+		for(int steps =1;steps<=100;steps++) {		
+			//模拟休息
+			if(Thread.currentThread().getName().equals("rabbit") && steps%10==0) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println(Thread.currentThread().getName()+"-->"+steps);
+			//比赛是否结束
+			boolean flag = gameOver(steps);
+			if(flag) {
+				break;
+			}
+		}
+	}
+	private boolean gameOver(int steps) {
+		if(winner!=null) { //存在胜利者
+			return true;
+		}else {
+			if(steps ==100) {
+				winner = Thread.currentThread().getName();
+				System.out.println("winner==>"+winner);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+}
