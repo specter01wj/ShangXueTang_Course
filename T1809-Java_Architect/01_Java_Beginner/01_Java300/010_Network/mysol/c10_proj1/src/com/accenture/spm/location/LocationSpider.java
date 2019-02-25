c9ad0a403a30eaff17e6537002e3f9ac448cfff2
specter01wj <specter01wj@gmail.com>
2019-02-25 00:44:14 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationSpider.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationSpider.java
@@ -1,10 +1,24 @@
 package com.accenture.spm.location;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LocationSpider {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		URL url =new URL("https://www.ebay.com/");
+		InputStream is = url.openStream();
+		BufferedReader br =new BufferedReader(new InputStreamReader(is,"UTF-8"));
+		String msg =null;
+		while(null!=(msg=br.readLine())) {
+			System.out.println(msg);
+		}
+		br.close();
 	}
 
 }
