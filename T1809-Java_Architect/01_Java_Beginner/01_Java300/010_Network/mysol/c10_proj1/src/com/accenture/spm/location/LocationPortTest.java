fe9b17d47134d6644a0b9886f6582a3b0aa863f8
specter01wj <specter01wj@gmail.com>
2019-02-24 22:13:25 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationPortTest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationPortTest.java
@@ -3,7 +3,12 @@ package com.accenture.spm.location;
 public class LocationPortTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		InetSocketAddress socketAddress = new InetSocketAddress("127.0.0.1",8080);
+		InetSocketAddress socketAddress2 = new InetSocketAddress("localhost",9000);
+		System.out.println(socketAddress.getHostName());
+		System.out.println(socketAddress.getAddress());
+		System.out.println(socketAddress2.getAddress());
+		System.out.println(socketAddress2.getPort());
 
 	}
 
