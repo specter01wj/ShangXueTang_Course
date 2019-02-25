1132cdf54bba5abe9bbaae3464b6f71d5d59c59c
specter01wj <specter01wj@gmail.com>
2019-02-24 20:24:12 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationIPTest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationIPTest.java
@@ -1,10 +1,14 @@
 package com.accenture.spm.location;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class LocationIPTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException {
+		InetAddress addr = InetAddress.getLocalHost();
+		System.out.println(addr.getHostAddress());
+		System.out.println(addr.getHostName());
 	}
 
 }
