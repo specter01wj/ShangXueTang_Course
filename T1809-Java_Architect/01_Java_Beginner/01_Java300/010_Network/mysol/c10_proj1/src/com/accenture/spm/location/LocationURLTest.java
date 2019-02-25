23f3e4f1554bce5b03ad3b556238f551d9bf781a
specter01wj <specter01wj@gmail.com>
2019-02-25 00:14:14 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationURLTest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/location/LocationURLTest.java
@@ -1,10 +1,21 @@
 package com.accenture.spm.location;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LocationURLTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws MalformedURLException {
+		URL url = new URL("http://www.baidu.com:80/index.html?uname=shsxt&age=18#a");
+		
+		System.out.println("协议:"+url.getProtocol());
+		System.out.println("域名|ip:"+url.getHost());
+		System.out.println("端口:"+url.getPort());
+		System.out.println("请求资源1:"+url.getFile());
+		System.out.println("请求资源2:"+url.getPath());
+		
+		System.out.println("参数:"+url.getQuery());
+		System.out.println("锚点:"+url.getRef());
 	}
 
 }
