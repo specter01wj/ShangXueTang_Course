e32834b86360d4531b0e1dfd5e7cd60f6214fc1b
specter01wj <specter01wj@gmail.com>
2019-02-26 18:50:09 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTypeServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTypeServer.java
@@ -1,10 +1,37 @@
 package com.accenture.spm.udp;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPTypeServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("接收方启动中.....");
+		// 1、使用DatagramSocket  指定端口 创建接收端
+		DatagramSocket server =new DatagramSocket(6666);
+		// 2、准备容器 封装成DatagramPacket 包裹
+		byte[] container =new byte[1024*60];
+		DatagramPacket packet = new DatagramPacket(container,0,container.length);
+		// 3、阻塞式接收包裹receive​(DatagramPacket p)
+		server.receive(packet); //阻塞式
+		// 4、分析数据    将字节数组还原为对应的类型
+		//    byte[]  getData​()
+		//                getLength​()
+		 byte[]  datas =packet.getData();
+		 int len = packet.getLength();		 
+		DataInputStream dis =new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(datas)));
+		//顺序与写出一致
+		String msg = dis.readUTF(); 
+		int age = dis.readInt();
+		boolean flag = dis.readBoolean();
+		char ch = dis.readChar();
+		System.out.println(msg+"-->"+flag);
+		// 5、释放资源
+		 server.close();
 	}
 
 }
