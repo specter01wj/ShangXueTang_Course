fc7ddd62cbfe8b7de16dbba90035d49fa6819e46
specter01wj <specter01wj@gmail.com>
2019-02-26 18:50:50 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTypeServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTypeServer.java
@@ -10,7 +10,7 @@ import java.net.DatagramSocket;
 public class UDPTypeServer {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("接收方启动中.....");
+		System.out.println("Receiving in progress.....");
 		// 1、使用DatagramSocket  指定端口 创建接收端
 		DatagramSocket server =new DatagramSocket(6666);
 		// 2、准备容器 封装成DatagramPacket 包裹
@@ -21,8 +21,8 @@ public class UDPTypeServer {
 		// 4、分析数据    将字节数组还原为对应的类型
 		//    byte[]  getData​()
 		//                getLength​()
-		 byte[]  datas =packet.getData();
-		 int len = packet.getLength();		 
+		byte[]  datas =packet.getData();
+		int len = packet.getLength();		 
 		DataInputStream dis =new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(datas)));
 		//顺序与写出一致
 		String msg = dis.readUTF(); 
