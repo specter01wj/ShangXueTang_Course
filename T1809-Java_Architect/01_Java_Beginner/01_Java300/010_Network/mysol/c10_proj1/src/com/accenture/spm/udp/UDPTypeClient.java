f7cc5ddc0e19a54f27414c304f73d27c12e6b513
specter01wj <specter01wj@gmail.com>
2019-02-26 18:47:24 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTypeClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTypeClient.java
@@ -1,10 +1,38 @@
 package com.accenture.spm.udp;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
 public class UDPTypeClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("Sending in progress.....");
+		 // 1、使用DatagramSocket  指定端口 创建发送端
+		DatagramSocket client =new DatagramSocket(8888);
+		 //2、准备数据 一定转成字节数组
+		//写出
+		ByteArrayOutputStream baos =new ByteArrayOutputStream();
+		DataOutputStream dos =new DataOutputStream(new BufferedOutputStream(baos));
+		//操作数据类型 +数据
+		dos.writeUTF("James Wang!!!");
+		dos.writeInt(18);
+		dos.writeBoolean(false);
+		dos.writeChar('a');
+		dos.flush();
+		byte[] datas =baos.toByteArray();	
+		 //3、 封装成DatagramPacket 包裹，需要指定目的地
+		DatagramPacket packet =new DatagramPacket(datas,0,datas.length,
+				new InetSocketAddress("localhost",6666));
+		//4、发送包裹send​(DatagramPacket p) * 
+		client.send(packet);
+		// 5、释放资源
+		client.close();
 	}
 
 }
