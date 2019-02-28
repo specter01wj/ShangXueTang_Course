7b91e3e0eae0414aa2b933a0a1f5095f7a823232
specter01wj <specter01wj@gmail.com>
2019-02-28 18:48:06 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/TalkSend.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/TalkSend.java
@@ -1,5 +1,53 @@
 package com.accenture.spm.udp;
 
-public class TalkSend {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
 
+public class TalkSend implements Runnable {
+
+	private DatagramSocket client ;
+	private BufferedReader reader;
+	private String toIP ;
+	private int toPort ;
+	
+	public TalkSend(int port,String toIP,int toPort) {
+		this.toIP = toIP;
+		this.toPort=toPort;
+		try {
+			client=new DatagramSocket(port);
+			reader =new BufferedReader(new InputStreamReader(System.in));
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			String data;
+			try {
+				data = reader.readLine();
+				byte[] datas = data.getBytes();
+				 //3、 封装成DatagramPacket 包裹，需要指定目的地
+				DatagramPacket packet =new DatagramPacket(datas,0,datas.length,
+						new InetSocketAddress(this.toIP,this.toPort));
+				//4、发送包裹send​(DatagramPacket p) * 
+				client.send(packet);
+				if(data.equals("bye")) {
+					break;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		// 5、释放资源
+		client.close();
+	}
+	
 }
