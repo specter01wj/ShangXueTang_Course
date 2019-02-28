0e0cc8211565f7f6fe6fd392d5ba9203db03c4e4
specter01wj <specter01wj@gmail.com>
2019-02-28 18:55:20 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/TalkTeacher.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/TalkTeacher.java
@@ -3,8 +3,9 @@ package com.accenture.spm.udp;
 public class TalkTeacher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Thread(new TalkReceive(9999,"student")).start();
+		
+		new Thread(new TalkSend(5555,"localhost",8888)).start();
 	}
 
 }
