b729e78a75ae6020b2d5add76e73ca088a952c4d
specter01wj <specter01wj@gmail.com>
2019-02-28 18:41:38 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTalkServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/UDPTalkServer.java
@@ -1,10 +1,36 @@
 package com.accenture.spm.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class UDPTalkServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("接收方启动中.....");
+		// 1、使用DatagramSocket  指定端口 创建接收端
+		DatagramSocket server =new DatagramSocket(6666);
+		
+		while(true) {
+			// 2、准备容器 封装成DatagramPacket 包裹
+			byte[] container =new byte[1024*60];
+			DatagramPacket packet = new DatagramPacket(container,0,container.length);
+			// 3、阻塞式接收包裹receive​(DatagramPacket p)
+			server.receive(packet); //阻塞式
+			// 4、分析数据
+			//    byte[]  getData​()
+			//                getLength​()
+			 byte[]  datas =packet.getData();
+			 int len = packet.getLength();
+			 String data=new String(datas,0,len);
+			 System.out.println(data);
+			 if(data.equals("bye")) {
+				 break;
+			 }
+		}
+		// 5、释放资源
+		 server.close();
 	}
 
 }
