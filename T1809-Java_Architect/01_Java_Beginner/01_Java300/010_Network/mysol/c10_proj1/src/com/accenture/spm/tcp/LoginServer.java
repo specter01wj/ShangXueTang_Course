4321c9c940b0f5465861d7d2d7bfc843de74ca85
specter01wj <specter01wj@gmail.com>
2019-03-01 17:49:57 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginServer.java
@@ -3,8 +3,31 @@ package com.accenture.spm.tcp;
 public class LoginServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		// 2、阻塞式等待连接 accept
+		Socket  client =server.accept(); 
+		System.out.println("一个客户端建立了连接");
+		// 3、操作: 输入输出流操作
+		DataInputStream dis =new DataInputStream(client.getInputStream());
+		String datas =dis.readUTF();
+		//分析
+		String[] dataArray = datas.split("&");
+		for(String info:dataArray) {
+			String[] userInfo =info.split("=");
+				if(userInfo[0].equals("uname")) {
+					System.out.println("你的用户名为:"+userInfo[1]);
+				}else if(userInfo[0].equals("upwd")) {
+					System.out.println("你的密码为:"+userInfo[1]);
+				}
+				
+		}
+		// 4、释放资源 
+		dis.close();
+		client.close();
+		
+		server.close();
 	}
 
 }
