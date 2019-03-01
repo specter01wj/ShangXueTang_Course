5ce8112c55c86490cefda4a2bc39d1b4b3032121
specter01wj <specter01wj@gmail.com>
2019-03-01 16:58:47 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Server.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Server.java
@@ -1,10 +1,28 @@
 package com.accenture.spm.tcp;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		// 2、阻塞式等待连接 accept
+		Socket  client =server.accept(); 
+		System.out.println("1 client connected!");
+		// 3、操作: 输入输出流操作
+		DataInputStream dis =new DataInputStream(client.getInputStream());
+		String data =dis.readUTF();
+		System.out.println(data);
+		// 4、释放资源 
+		dis.close();
+		client.close();
+		
+		server.close();
 	}
 
 }
