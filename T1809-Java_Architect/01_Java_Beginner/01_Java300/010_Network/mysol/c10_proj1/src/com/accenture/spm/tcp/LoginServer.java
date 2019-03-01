a64c06fda9ca7c1b225c11dae6e6d6ccd37847bc
specter01wj <specter01wj@gmail.com>
2019-03-01 17:50:39 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginServer.java
@@ -1,12 +1,13 @@
 package com.accenture.spm.tcp;
 
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class LoginServer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("-----Server-----");
 		// 1、指定端口 使用ServerSocket创建服务器
 		ServerSocket server =new ServerSocket(8888);
