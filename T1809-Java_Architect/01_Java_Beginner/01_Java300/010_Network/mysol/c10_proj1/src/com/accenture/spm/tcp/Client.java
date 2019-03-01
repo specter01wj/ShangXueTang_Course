6b1162b83f24073daa47840668a68317a38b1358
specter01wj <specter01wj@gmail.com>
2019-03-01 17:03:07 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Client.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Client.java
@@ -3,8 +3,17 @@ package com.accenture.spm.tcp;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Client-----");
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、操作: 输入输出流操作
+		DataOutputStream dos =new DataOutputStream(client.getOutputStream());
+		String data ="hello";
+		dos.writeUTF(data);
+		dos.flush();
+		//3、释放资源 
+		dos.close();
+		client.close();
 	}
 
 }
