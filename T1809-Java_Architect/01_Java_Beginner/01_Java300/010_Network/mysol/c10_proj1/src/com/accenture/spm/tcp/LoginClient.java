216752db20c3f36fb3d9d821182e5684297fd7cd
specter01wj <specter01wj@gmail.com>
2019-03-01 17:55:03 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginClient.java
@@ -3,8 +3,22 @@ package com.accenture.spm.tcp;
 public class LoginClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Client-----");
+		BufferedReader console =new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("请输入用户名:");
+		String uname =console.readLine();
+		System.out.print("请输入密码:");
+		String upwd =console.readLine();
+		
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、操作: 输入输出流操作
+		DataOutputStream dos =new DataOutputStream(client.getOutputStream());		
+		dos.writeUTF("uname="+uname+"&"+"upwd="+upwd);
+		dos.flush();
+		//3、释放资源 
+		dos.close();
+		client.close();
 	}
 
 }
