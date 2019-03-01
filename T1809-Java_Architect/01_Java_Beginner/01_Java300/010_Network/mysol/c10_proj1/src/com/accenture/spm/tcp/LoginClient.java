f2934c731e3a4d2a9bea8de9c38b7aecd02b8992
specter01wj <specter01wj@gmail.com>
2019-03-01 17:55:17 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginClient.java
@@ -1,5 +1,10 @@
 package com.accenture.spm.tcp;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class LoginClient {
 
 	public static void main(String[] args) {
