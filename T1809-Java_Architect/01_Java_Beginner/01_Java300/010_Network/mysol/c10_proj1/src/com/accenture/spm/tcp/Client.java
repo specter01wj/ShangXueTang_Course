cca04252ad9ed2a0bb7284f8d680d67d2adb8a66
specter01wj <specter01wj@gmail.com>
2019-03-01 17:03:44 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Client.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Client.java
@@ -1,14 +1,19 @@
 package com.accenture.spm.tcp;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		System.out.println("-----Client-----");
 		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
 		Socket client =new Socket("localhost",8888);
 		//2、操作: 输入输出流操作
 		DataOutputStream dos =new DataOutputStream(client.getOutputStream());
-		String data ="hello";
+		String data ="James Wang!!!";
 		dos.writeUTF(data);
 		dos.flush();
 		//3、释放资源 
