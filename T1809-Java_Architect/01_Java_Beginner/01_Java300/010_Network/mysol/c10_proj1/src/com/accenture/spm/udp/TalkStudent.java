3e1c8527630aa6ee4540986d11a26694350ff9f9
specter01wj <specter01wj@gmail.com>
2019-02-28 19:12:27 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/TalkStudent.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/udp/TalkStudent.java
@@ -4,6 +4,7 @@ public class TalkStudent {
 
 	public static void main(String[] args) {
 		new Thread(new TalkSend(7777,"localhost",9999)).start();	
+		
 		new Thread(new TalkReceive(8888,"teacher")).start();
 	}
 
