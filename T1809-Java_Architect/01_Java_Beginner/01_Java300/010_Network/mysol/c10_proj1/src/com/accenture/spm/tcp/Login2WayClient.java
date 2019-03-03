4d85f4b083fb75f6bfcb2d43925a2f7c3592e968
specter01wj <specter01wj@gmail.com>
2019-03-03 00:30:08 -0500

    02/22/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/Login2WayClient.java
@@ -0,0 +1,35 @@
+package com.accenture.spm.tcp;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Login2WayClient {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----Client-----");
+		BufferedReader console =new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("请输入用户名:");
+		String uname =console.readLine();
+		System.out.print("请输入密码:");
+		String upwd =console.readLine();
+		
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、操作: 输入输出流操作
+		DataOutputStream dos =new DataOutputStream(client.getOutputStream());		
+		dos.writeUTF("uname="+uname+"&"+"upwd="+upwd);
+		dos.flush();
+		
+		DataInputStream dis =new DataInputStream(client.getInputStream());
+		String result =dis.readUTF();		
+		System.out.println(result);
+		//3、释放资源 
+		dos.close();
+		client.close();
+	}
+
+}
