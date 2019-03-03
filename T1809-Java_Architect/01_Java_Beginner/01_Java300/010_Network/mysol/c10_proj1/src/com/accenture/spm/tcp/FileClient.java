5f97cf1fe6aec4fad926b06e048abc8381d9cf0d
specter01wj <specter01wj@gmail.com>
2019-03-03 02:12:04 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/FileClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/FileClient.java
@@ -1,10 +1,33 @@
 package com.accenture.spm.tcp;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class FileClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		System.out.println("-----Client-----");
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、操作: 拷贝 上传
+		InputStream is =new BufferedInputStream(new FileInputStream("img/ndl.png"));
+		OutputStream os =new BufferedOutputStream(client.getOutputStream());
+		byte[] flush =new byte[1024];
+		int len = -1;
+		while((len=is.read(flush))!=-1) {
+			os.write(flush,0,len);
+		}
+		os.flush();
+		//3、释放资源 
+		os.close();
+		is.close();
+		client.close();
 	}
 
 }
