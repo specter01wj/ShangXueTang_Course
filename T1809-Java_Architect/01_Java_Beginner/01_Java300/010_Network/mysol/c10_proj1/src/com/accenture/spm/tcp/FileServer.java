e3c8e24ee5352aa8fbe3dcacf9bba48f5d54e5d9
specter01wj <specter01wj@gmail.com>
2019-03-03 02:15:56 -0500

    02/22/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/FileServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/FileServer.java
@@ -3,8 +3,29 @@ package com.accenture.spm.tcp;
 public class FileServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		// 2、阻塞式等待连接 accept
+		Socket  client =server.accept(); 
+		System.out.println("一个客户端建立了连接");
+		// 3、操作: 文件拷贝 存储
+		InputStream is =new BufferedInputStream(client.getInputStream());
+		OutputStream os =new BufferedOutputStream(new FileOutputStream("src/tcp.png"));
+		byte[] flush =new byte[1024];
+		int len = -1;
+		while((len=is.read(flush))!=-1) {
+			os.write(flush,0,len);
+		}
+		os.flush();
+		//3、释放资源 
+		os.close();
+		is.close();
+		
+		// 4、释放资源 
+		client.close();
+		
+		server.close();
 	}
 
 }
