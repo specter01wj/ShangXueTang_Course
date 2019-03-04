2986b2a8c4f9363905c0864f82a17e3ec3d7b516
specter01wj <specter01wj@gmail.com>
2019-03-03 23:23:30 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginMultiClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginMultiClient.java
@@ -1,10 +1,16 @@
 package com.accenture.spm.tcp;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class LoginMultiClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		System.out.println("-----Client-----");
 		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
 		Socket client =new Socket("localhost",8888);
