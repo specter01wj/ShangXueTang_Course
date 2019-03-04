5f16b861691e656a14f5f123d5cc812783662fca
specter01wj <specter01wj@gmail.com>
2019-03-03 23:28:34 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginMultiServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginMultiServer.java
@@ -18,5 +18,98 @@ public class LoginMultiServer {
 		}
 		server.close();
 	}
+	
+	static class Channel implements Runnable{
+		private Socket  client;
+		//输入流
+		private DataInputStream dis;
+		//输出流
+		private DataOutputStream dos;
+		public Channel(Socket  client) {
+			this.client = client;
+			try {
+				//输入
+				dis = new DataInputStream(client.getInputStream());
+				//输出
+				dos =new DataOutputStream(client.getOutputStream());	
+			} catch (IOException e) {
+				e.printStackTrace();
+				release();
+			}
+			
+		}
+		
+		//接收数据
+		private String receive() {
+			String datas ="";
+			try {
+				datas = dis.readUTF();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return datas;
+		}
+		//释放资源
+		private void release() {
+			// 4、释放资源 
+			try {
+				if(null != dos) {
+					dos.close();					
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				if(null != dis) {
+					dis.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				if(null != client) {
+					client.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//发送数据
+		private void send(String msg) {
+			try {
+				dos.writeUTF(msg);
+				dos.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		@Override
+		public void run() {						
+			// 3、操作: 输入输出流操作					
+			String uname ="";
+			String upwd ="";
+			//分析
+			String[] dataArray = receive().split("&");
+			for(String info:dataArray) {
+					String[] userInfo =info.split("=");
+					if(userInfo[0].equals("uname")) {
+						System.out.println("你的用户名为:"+userInfo[1]);
+						uname = userInfo[1];
+					}else if(userInfo[0].equals("upwd")) {
+						System.out.println("你的密码为:"+userInfo[1]);
+						upwd = userInfo[1];
+					}				
+			}					
+			if(uname.equals("shsxt") && upwd.equals("laopei")) { //成功
+				send("登录成功，欢迎回来");
+			}else { //失败
+				send("用户名或密码错误");
+			}
+			
+			release();
+			
+		}
+		
+	}
 
 }
