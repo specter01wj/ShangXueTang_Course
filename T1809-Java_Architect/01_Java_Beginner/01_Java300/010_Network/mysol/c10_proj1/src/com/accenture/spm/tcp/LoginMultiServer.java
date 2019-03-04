c8b56a0c40a42ec8e0af3d256f885f91cc2bc88b
specter01wj <specter01wj@gmail.com>
2019-03-03 23:28:01 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginMultiServer.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj1/src/com/accenture/spm/tcp/LoginMultiServer.java
@@ -1,10 +1,22 @@
 package com.accenture.spm.tcp;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class LoginMultiServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		boolean isRunning =true;;
+		// 2、阻塞式等待连接 accept
+		while(isRunning) {
+			Socket  client =server.accept(); 
+			System.out.println("一个客户端建立了连接");
+			new Thread(new Channel(client)).start();
+		}
+		server.close();
 	}
 
 }
