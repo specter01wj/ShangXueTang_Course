678b7909286ec7d75f631781c5ac19910dada99d
specter01wj <specter01wj@gmail.com>
2019-03-04 18:46:58 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/singleuser/Client.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/singleuser/Client.java
@@ -1,10 +1,33 @@
 package com.accenture.spm.chat.singleuser;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		System.out.println("-----Client-----");
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、客户端发送消息
+		BufferedReader console =new BufferedReader(new InputStreamReader(System.in));
+		String msg = console.readLine();
+		DataOutputStream dos =new DataOutputStream(client.getOutputStream());		
+		dos.writeUTF(msg);
+		dos.flush();
+		//3、获取消息
+		DataInputStream dis =new DataInputStream(client.getInputStream());
+		msg =dis.readUTF();
+		System.out.println(msg);
+		
+		dos.close();
+		dis.close();
+		client.close();
 	}
 
 }
