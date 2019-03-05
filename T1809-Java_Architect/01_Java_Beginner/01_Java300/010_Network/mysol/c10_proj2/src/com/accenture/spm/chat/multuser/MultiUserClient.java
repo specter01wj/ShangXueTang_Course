44718c2210bfd7f8f8b922e74ec3f70c1c36266f
specter01wj <specter01wj@gmail.com>
2019-03-05 18:43:43 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/multuser/MultiUserClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/multuser/MultiUserClient.java
@@ -3,8 +3,25 @@ package com.accenture.spm.chat.multuser;
 public class MultiUserClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Client-----");
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、客户端发送消息
+		BufferedReader console =new BufferedReader(new InputStreamReader(System.in));
+		DataOutputStream dos =new DataOutputStream(client.getOutputStream());		
+		DataInputStream dis =new DataInputStream(client.getInputStream());
+		boolean isRunning = true;
+		while(isRunning) {
+			String msg = console.readLine();
+			dos.writeUTF(msg);
+			dos.flush();
+			//3、获取消息
+			msg =dis.readUTF();
+			System.out.println(msg);
+		}
+		dos.close();
+		dis.close();
+		client.close();
 	}
 
 }
