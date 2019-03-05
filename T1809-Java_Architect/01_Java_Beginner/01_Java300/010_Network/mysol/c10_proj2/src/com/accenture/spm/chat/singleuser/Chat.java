233f280820346b705aebebe0d3afddf442811a7d
specter01wj <specter01wj@gmail.com>
2019-03-05 11:51:16 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/singleuser/Chat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/singleuser/Chat.java
@@ -1,10 +1,32 @@
 package com.accenture.spm.chat.singleuser;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Chat {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		// 2、阻塞式等待连接 accept
+		Socket  client =server.accept(); 
+		System.out.println("一个客户端建立了连接");
+		
+		//3、接收消息
+		DataInputStream dis =new DataInputStream(client.getInputStream());
+		String msg =dis.readUTF();
+		//4、返回消息
+		DataOutputStream dos =new DataOutputStream(client.getOutputStream());		
+		dos.writeUTF(msg);
+		//释放资源
+		dos.flush();
+		dos.close();
+		dis.close();
+		client.close();
 	}
 
 }
