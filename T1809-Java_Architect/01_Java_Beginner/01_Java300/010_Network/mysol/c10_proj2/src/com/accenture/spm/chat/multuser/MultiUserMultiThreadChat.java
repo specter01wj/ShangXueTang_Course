728fccc9eb98d7462e2f5f73c3c0c12d1fcc72b2
specter01wj <specter01wj@gmail.com>
2019-03-05 18:48:43 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/multuser/MultiUserMultiThreadChat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/multuser/MultiUserMultiThreadChat.java
@@ -1,10 +1,71 @@
 package com.accenture.spm.chat.multuser;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MultiUserMultiThreadChat {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		// 2、阻塞式等待连接 accept
+		while(true) {
+			Socket  client =server.accept(); 
+			System.out.println("一个客户端建立了连接");
+			
+			new Thread(()->{
+				DataInputStream dis=null;
+				DataOutputStream dos=null;
+				try {
+					dis = new DataInputStream(client.getInputStream());
+					dos =new DataOutputStream(client.getOutputStream());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}					
+				boolean isRunning = true;
+				while(isRunning) {
+					//3、接收消息
+					String msg;
+					try {
+						msg = dis.readUTF();
+						//4、返回消息
+						dos.writeUTF(msg);
+						//释放资源
+						dos.flush();
+					} catch (IOException e) {
+						//e.printStackTrace();
+						isRunning = false; //停止线程
+					}					
+				}
+				try {
+					if(null==dos) {
+						dos.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				try {
+					if(null==dis) {
+						dis.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				try {
+					if(null==client) {
+						client.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			
+			}).start(); 
+			
+			}
 	}
 
 }
