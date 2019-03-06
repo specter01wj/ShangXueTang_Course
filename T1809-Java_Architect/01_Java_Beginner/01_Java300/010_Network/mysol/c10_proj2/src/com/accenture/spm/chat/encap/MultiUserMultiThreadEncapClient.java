4dca7acbb799f1e3e9a30c4f3706c20e33ff1f86
specter01wj <specter01wj@gmail.com>
2019-03-06 17:56:22 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/MultiUserMultiThreadEncapClient.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/MultiUserMultiThreadEncapClient.java
@@ -1,10 +1,16 @@
 package com.accenture.spm.chat.encap;
 
+import java.net.Socket;
+
 public class MultiUserMultiThreadEncapClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----Client-----");
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、客户端发送消息
+		new Thread(new Send(client)).start();  
+		new Thread(new Receive(client)).start();
 	}
 
 }
