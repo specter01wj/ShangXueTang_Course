0c7f17550ecf1a8216188104e0ed1898f6e68f7d
specter01wj <specter01wj@gmail.com>
2019-03-06 17:57:21 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/SxtUtils.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/SxtUtils.java
@@ -1,5 +1,19 @@
 package com.accenture.spm.chat.encap;
 
+import java.io.Closeable;
+
 public class SxtUtils {
 
+	public static void close(Closeable... targets ) {
+		for(Closeable target:targets) {
+			try {
+				if(null!=target) {
+					target.close();
+				}
+			}catch(Exception e) {
+				
+			}
+		}
+	}
+	
 }
