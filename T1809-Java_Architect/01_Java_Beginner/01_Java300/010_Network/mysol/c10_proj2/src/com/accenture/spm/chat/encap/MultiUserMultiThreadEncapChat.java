65210dea164b5f6fa392ce0974a2c6b8dfa90324
specter01wj <specter01wj@gmail.com>
2019-03-06 17:51:00 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/MultiUserMultiThreadEncapChat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/MultiUserMultiThreadEncapChat.java
@@ -15,7 +15,7 @@ public class MultiUserMultiThreadEncapChat {
 		// 2、阻塞式等待连接 accept
 		while(true) {
 			Socket  client =server.accept(); 
-			System.out.println("一个客户端建立了连接");
+			System.out.println("1 client connected!");
 			new Thread(new Channel(client)).start();			
 		}	
 	}
