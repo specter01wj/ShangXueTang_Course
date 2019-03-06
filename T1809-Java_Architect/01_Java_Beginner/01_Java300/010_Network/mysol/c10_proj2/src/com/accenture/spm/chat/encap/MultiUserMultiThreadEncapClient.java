5606d6d0a707fad705a73138b3c32e9f21313d7c
specter01wj <specter01wj@gmail.com>
2019-03-06 17:55:07 -0500

    03/03/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/encap/MultiUserMultiThreadEncapClient.java
@@ -0,0 +1,10 @@
+package com.accenture.spm.chat.encap;
+
+public class MultiUserMultiThreadEncapClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
