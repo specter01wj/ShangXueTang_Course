ff6a78a398851f0d11fb6c908784ec203fb4c435
specter01wj <specter01wj@gmail.com>
2019-03-08 14:41:47 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/collection/Receive.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/collection/Receive.java
@@ -11,6 +11,7 @@ public class Receive implements Runnable {
 	private DataInputStream dis ;
 	private Socket client;
 	private boolean isRunning;
+	
 	public Receive(Socket client) {
 		this.client = client;
 		this.isRunning = true;
@@ -21,7 +22,7 @@ public class Receive implements Runnable {
 			release();
 		}
 	}
-	//接收消息
+	
 	private String receive() {
 		String msg ="";
 		try {
@@ -42,7 +43,7 @@ public class Receive implements Runnable {
 			}
 		}
 	}
-	//释放资源
+	
 	private void release() {
 		this.isRunning = false;
 		SxtUtils.close(dis,client);
