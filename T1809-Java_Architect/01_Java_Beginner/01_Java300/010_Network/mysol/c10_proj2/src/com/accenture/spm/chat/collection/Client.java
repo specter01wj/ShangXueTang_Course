e1ead6b034646a2336efaa75e1bab306f902ec44
specter01wj <specter01wj@gmail.com>
2019-03-08 13:37:07 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/collection/Client.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/collection/Client.java
@@ -3,8 +3,15 @@ package com.accenture.spm.chat.collection;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Client-----");
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("请输入用户名:");
+		String name =br.readLine();
+		//1、建立连接: 使用Socket创建客户端 +服务的地址和端口
+		Socket client =new Socket("localhost",8888);
+		//2、客户端发送消息
+		new Thread(new Send(client,name)).start();  
+		new Thread(new Receive(client)).start();
 	}
 
 }
