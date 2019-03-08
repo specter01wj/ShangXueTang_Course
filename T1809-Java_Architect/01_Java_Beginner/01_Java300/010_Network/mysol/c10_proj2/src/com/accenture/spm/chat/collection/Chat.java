9f563a35d3a945b5103986c2190b00f998b9b5d2
specter01wj <specter01wj@gmail.com>
2019-03-08 13:36:17 -0500

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/collection/Chat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/collection/Chat.java
@@ -1,8 +1,19 @@
 package com.accenture.spm.chat.collection;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import com.accenture.spm.chat.encap.SxtUtils;
+
 public class Chat {
 
-	public static void main(String[] args) {
+	private static CopyOnWriteArrayList<Channel> all =new CopyOnWriteArrayList<Channel>();
+	
+	public static void main(String[] args) throws IOException {
 		System.out.println("-----Server-----");
 		// 1、指定端口 使用ServerSocket创建服务器
 		ServerSocket server =new ServerSocket(8888);
@@ -14,6 +25,85 @@ public class Chat {
 			all.add(c); //管理所有的成员
 			new Thread(c).start();			
 		}		
+	}
+	
+	static class Channel implements Runnable{
+		private DataInputStream dis;
+		private DataOutputStream dos;
+		private Socket  client;			
+		private boolean isRunning;
+		private String name;
+		public Channel(Socket  client) {
+			this.client = client;
+			try {
+				dis = new DataInputStream(client.getInputStream());
+				dos =new DataOutputStream(client.getOutputStream());
+				isRunning =true;
+				//获取名称
+				this.name =receive();
+				//欢迎你的到来
+				this.send("欢迎你的到来");
+				sendOthers(this.name+"来了shsxt聊天室",true);
+			} catch (IOException e) {
+				System.out.println("---1------");
+				release();					
+			}			
+		}
+		//接收消息
+		private String receive() {
+			String msg ="";
+			try {
+				msg =dis.readUTF();
+			} catch (IOException e) {
+				System.out.println("---2------");
+				release();
+			}
+			return msg;
+		}
+		//发送消息
+		private void send(String msg) {
+			try {
+				dos.writeUTF(msg);
+				dos.flush();
+			} catch (IOException e) {
+				System.out.println("---3------");
+				release();
+			}
+		}
+		/**
+		 * 群聊：获取自己的消息，发给其他人
+		 * @param msg
+		 */
+		private void sendOthers(String msg,boolean isSys) {
+			for(Channel other: all) {
+				if(other==this) { //自己
+					continue;
+				}
+				if(!isSys) {
+					other.send(this.name +"对所有人说:"+msg);//群聊消息
+				}else {
+					other.send(msg); //系统消息
+				}
+			}
+		}
+		//释放资源
+		private void release() {
+			this.isRunning = false;
+			SxtUtils.close(dis,dos,client);
+			//退出
+			all.remove(this);
+			sendOthers(this.name+"离开大家庭...",true);
+		}
+		@Override
+		public void run() {
+			while(isRunning) {
+				String msg = receive() ;
+				if(!msg.equals("")) {
+					//send(msg);
+					sendOthers(msg,false);
+				}
+			}
+		}
 	}
 
 }
