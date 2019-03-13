bcabd5a2111eaca5197ef562ed13ad6ac78c206e
specter01wj <specter01wj@gmail.com>
2019-03-13 17:56:11 -0400

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/ultimate/Chat.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/ultimate/Chat.java
@@ -3,8 +3,17 @@ package com.accenture.spm.chat.ultimate;
 public class Chat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("-----Server-----");
+		// 1、指定端口 使用ServerSocket创建服务器
+		ServerSocket server =new ServerSocket(8888);
+		// 2、阻塞式等待连接 accept
+		while(true) {
+			Socket  client =server.accept(); 
+			System.out.println("一个客户端建立了连接");
+			Channel c =new Channel(client);
+			all.add(c); //管理所有的成员
+			new Thread(c).start();			
+		}	
 	}
 
 }
