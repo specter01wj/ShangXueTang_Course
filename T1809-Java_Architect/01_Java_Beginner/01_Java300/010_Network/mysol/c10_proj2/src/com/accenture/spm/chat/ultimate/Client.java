f6766b92e8ccfc0477272e17db01dc41ddb4abbf
specter01wj <specter01wj@gmail.com>
2019-03-13 18:02:10 -0400

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/ultimate/Client.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/010_Network/mysol/c10_proj2/src/com/accenture/spm/chat/ultimate/Client.java
@@ -1,12 +1,13 @@
 package com.accenture.spm.chat.ultimate;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("-----Client-----");
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Your Name:");
