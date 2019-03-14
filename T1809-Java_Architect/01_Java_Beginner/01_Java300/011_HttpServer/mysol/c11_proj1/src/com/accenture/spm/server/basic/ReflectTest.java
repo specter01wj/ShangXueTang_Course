7e675873a23a0f78a1f7b878d641c4ef62c3122d
specter01wj <specter01wj@gmail.com>
2019-03-14 18:41:34 -0400

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/ReflectTest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/ReflectTest.java
@@ -1,8 +1,10 @@
 package com.accenture.spm.server.basic;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ReflectTest {
 
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		
 		Class clz1 = new Iphone().getClass();
 		
@@ -10,9 +12,10 @@ public class ReflectTest {
 		
 		Class clz3 = Class.forName("com.accenture.spm.server.basic.Iphone");
 		
-		Iphone iphone2 =(Iphone)clz3.newInstance();
-		System.out.println(iphone2);
-		
+//		Iphone iphone2 =(Iphone)clz3.newInstance();
+//		System.out.println(iphone2);
+		Iphone iphone3 = (Iphone)clz3.getConstructor().newInstance();
+		System.out.println(iphone3);
 	}
 
 }
