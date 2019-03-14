f412327552cf1f2ee7affeebafa4183796701bcd
specter01wj <specter01wj@gmail.com>
2019-03-14 18:37:35 -0400

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/ReflectTest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/ReflectTest.java
@@ -3,8 +3,21 @@ package com.accenture.spm.server.basic;
 public class ReflectTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Class clz1 = new Iphone().getClass();
+		
+		Class clz2 = Iphone.class;
+		
+		Class clz3 = Class.forName("com.accenture.spm.server.basic.Iphone");
+		
 	}
 
+}
+
+class Iphone {
+	
+	public Iphone() {
+		
+	}
+	
 }
