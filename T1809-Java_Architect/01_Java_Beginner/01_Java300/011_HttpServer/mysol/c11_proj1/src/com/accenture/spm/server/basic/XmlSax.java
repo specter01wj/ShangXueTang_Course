0c90063125a0d7de5c698367f2589ec47f52df4a
specter01wj <specter01wj@gmail.com>
2019-03-16 20:49:30 -0400

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/XmlSax.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/XmlSax.java
@@ -3,8 +3,18 @@ package com.accenture.spm.server.basic;
 public class XmlSax {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//SAX解析
+		//1、获取解析工厂
+		SAXParserFactory factory=SAXParserFactory.newInstance();
+		//2、从解析工厂获取解析器
+		SAXParser parse =factory.newSAXParser();
+		//3、编写处理器
+		//4、加载文档 Document 注册处理器
+		PHandler handler=new PHandler();
+		//5、解析
+		parse.parse(Thread.currentThread().getContextClassLoader()
+		.getResourceAsStream("com/sxt/server/basic/p.xml")
+		,handler);
 	}
 
 }
