03a43f7937e115dff29328718c1876a33ed644a7
specter01wj <specter01wj@gmail.com>
2019-03-18 01:20:12 -0400

    03/03/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/XmlSaxPerson.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/basic/XmlSaxPerson.java
@@ -1,10 +1,27 @@
 package com.accenture.spm.server.basic;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
 public class XmlSaxPerson {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		//SAX解析
+		//1、获取解析工厂
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		//2、从解析工厂获取解析器
+		SAXParser parse = factory.newSAXParser();
+		//3、编写处理器
+		//4、加载文档 Document 注册处理器
+		PHandler handler=new PHandler();
+		//5、解析
+		parse.parse(Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("p.xml"), handler);
 	}
 
 }
