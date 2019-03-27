d864df29c69c41f146463540ba23e8c2f6367342
specter01wj <specter01wj@gmail.com>
2019-03-27 18:37:19 -0400

    03/27/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/servlet/RegisterServlet.java
@@ -0,0 +1,11 @@
+package com.accenture.spm.server.servlet;
+
+public class RegisterServlet implements Servlet {
+
+	@Override
+	public void service() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
