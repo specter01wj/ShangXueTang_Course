12fa417b85ce20c6c72d627c99887aada47930dd
specter01wj <specter01wj@gmail.com>
2019-03-27 18:38:10 -0400

    03/27/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/servlet/LoginServlet.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/servlet/LoginServlet.java
@@ -4,8 +4,7 @@ public class LoginServlet implements Servlet {
 
 	@Override
 	public void service() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Login Servlet");
 	}
 
 }
--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/servlet/RegisterServlet.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/servlet/RegisterServlet.java
@@ -4,8 +4,7 @@ public class RegisterServlet implements Servlet {
 
 	@Override
 	public void service() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Register Servlet");
 	}
 
 }
