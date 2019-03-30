0d80930cebc6df7301e50906aaf08d85ba6a9e83
specter01wj <specter01wj@gmail.com>
2019-03-30 01:18:57 -0400

    03/27/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerConnect.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerConnect.java
@@ -6,5 +6,35 @@ public class ServerConnect {
 		ServerConnect server = new ServerConnect();
 		server.start();
 	}
+	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			 receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("服务器启动失败....");
+		}
+	}
+	
+	public void receive() {
+		try {
+			Socket client = serverSocket.accept();
+			System.out.println("一个客户端建立了连接....");
+			//获取请求协议
+			InputStream is =client.getInputStream();
+			byte[] datas = new byte[1024*1024];
+			int len = is.read(datas);
+			String requestInfo = new String(datas,0,len);
+			System.out.println(requestInfo);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("客户端错误");
+		}
+	}
+	
+	public void stop() {
+		
+	}
 
 }
