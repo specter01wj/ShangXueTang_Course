c6599638769945192e7e02f811e3f1c77ad0ed46
specter01wj <specter01wj@gmail.com>
2019-04-03 18:33:59 -0400

    03/27/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerReturn.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerReturn.java
@@ -1,5 +1,10 @@
 package com.accenture.spm.server.serversocket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerReturn {
 
 	public static void main(String[] args) {
@@ -7,6 +12,36 @@ public class ServerReturn {
 		server.start();
 	}
 	
+	private ServerSocket serverSocket;
 	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("ServerSocket Failure....");
+		}
+	}
+	
+	public void receive() {
+		try {
+			Socket client = serverSocket.accept();
+			System.out.println("1 client Connected....");
+			//获取请求协议
+			InputStream is =client.getInputStream();
+			byte[] datas = new byte[1024*10];
+			int len = is.read(datas);
+			String requestInfo = new String(datas,0,len);
+			System.out.println(requestInfo);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Client Error!");
+		}
+	}
+	
+	public void stop() {
+		
+	}
 
 }
