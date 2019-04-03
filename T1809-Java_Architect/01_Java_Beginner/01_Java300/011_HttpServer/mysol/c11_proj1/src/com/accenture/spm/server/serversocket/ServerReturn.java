7c97e7b9de356bbc616a21bf7251066a47e15b56
specter01wj <specter01wj@gmail.com>
2019-04-03 18:36:03 -0400

    03/27/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerReturn.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerReturn.java
@@ -34,6 +34,48 @@ public class ServerReturn {
 			int len = is.read(datas);
 			String requestInfo = new String(datas,0,len);
 			System.out.println(requestInfo);
+			
+			StringBuilder content =new StringBuilder();
+			content.append("<html>");
+			content.append("<head>");
+			content.append("<title>");
+			content.append("服务器响应成功");
+			content.append("</title>");
+			content.append("</head>");
+			content.append("<body>");
+			content.append("shsxt server终于回来了。。。。");
+			content.append("</body>");
+			content.append("</html>");
+			int size = content.toString().getBytes().length; //必须获取字节长度
+			StringBuilder responseInfo =new StringBuilder();
+			String blank =" ";
+			String CRLF = "\r\n";
+			//返回
+			//1、响应行: HTTP/1.1 200 OK
+			responseInfo.append("HTTP/1.1").append(blank);
+			responseInfo.append(200).append(blank);
+			responseInfo.append("OK").append(CRLF);
+			//2、响应头(最后一行存在空行):
+			/*
+			 Date:Mon,31Dec209904:25:57GMT
+			Server:shsxt Server/0.0.1;charset=GBK
+			Content-type:text/html
+			Content-length:39725426
+			 */
+			responseInfo.append("Date:").append(new Date()).append(CRLF);
+			responseInfo.append("Server:").append("shsxt Server/0.0.1;charset=GBK").append(CRLF);
+			responseInfo.append("Content-type:text/html").append(CRLF);
+			responseInfo.append("Content-length:").append(size).append(CRLF);
+			responseInfo.append(CRLF);
+			//3、正文
+			responseInfo.append(content.toString());
+			
+			//写出到客户端			
+			BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+			bw.write(responseInfo.toString());
+			bw.flush();
+			
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Client Error!");
