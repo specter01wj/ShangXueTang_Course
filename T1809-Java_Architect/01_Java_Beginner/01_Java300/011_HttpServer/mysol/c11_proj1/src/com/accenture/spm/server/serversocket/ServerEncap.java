8e8905bf65b7df454a3a7c912b72c9cc57539448
specter01wj <specter01wj@gmail.com>
2019-04-04 00:19:09 -0400

    04/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncap.java
@@ -1,10 +1,91 @@
 package com.accenture.spm.server.serversocket;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class ServerEncap {
 
 	public static void main(String[] args) {
 		ServerEncap server = new ServerEncap();
 		server.start();
+	}
+	
+	private ServerSocket serverSocket;
+	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("ServerSocket Failure....");
+		}
+	}
+	
+	public void receive() {
+		try {
+			Socket client = serverSocket.accept();
+			System.out.println("1 client Connected....");
+			//获取请求协议
+			InputStream is =client.getInputStream();
+			byte[] datas = new byte[1024*10];
+			int len = is.read(datas);
+			String requestInfo = new String(datas,0,len);
+			System.out.println(requestInfo);
+			
+			StringBuilder content =new StringBuilder();
+			content.append("<html>");
+			content.append("<head>");
+			content.append("<title>");
+			content.append("Server Response Success!!!");
+			content.append("</title>");
+			content.append("</head>");
+			content.append("<body>");
+			content.append("James's server is Back!!!");
+			content.append("</body>");
+			content.append("</html>");
+			int size = content.toString().getBytes().length; //必须获取字节长度
+			StringBuilder responseInfo =new StringBuilder();
+			String blank =" ";
+			String CRLF = "\r\n";
+			//返回
+			//1、响应行: HTTP/1.1 200 OK
+			responseInfo.append("HTTP/1.1").append(blank);
+			responseInfo.append(200).append(blank);
+			responseInfo.append("OK").append(CRLF);
+			//2、响应头(最后一行存在空行):
+			/*
+			 Date:Mon,31Dec209904:25:57GMT
+			Server:shsxt Server/0.0.1;charset=GBK
+			Content-type:text/html
+			Content-length:39725426
+			 */
+			responseInfo.append("Date:").append(new Date()).append(CRLF);
+			responseInfo.append("Server:").append("shsxt Server/0.0.1;charset=GBK").append(CRLF);
+			responseInfo.append("Content-type:text/html").append(CRLF);
+			responseInfo.append("Content-length:").append(size).append(CRLF);
+			responseInfo.append(CRLF);
+			//3、正文
+			responseInfo.append(content.toString());
+			
+			//写出到客户端			
+			BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+			bw.write(responseInfo.toString());
+			bw.flush();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Client Error!");
+		}
+	}
+	
+	public void stop() {
+		
 	}
 
 }
