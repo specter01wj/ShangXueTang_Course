33456172c51378c884086ce115c94f995ed76011
specter01wj <specter01wj@gmail.com>
2019-04-04 00:16:38 -0400

    04/02/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncap.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncap.java
@@ -3,8 +3,8 @@ package com.accenture.spm.server.serversocket;
 public class ServerEncap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerEncap server = new ServerEncap();
+		server.start();
 	}
 
 }
