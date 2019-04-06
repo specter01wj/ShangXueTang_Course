e929ac96aa2da9d4a5b31bbec899a875a9c99666
specter01wj <specter01wj@gmail.com>
2019-04-06 13:27:58 -0400

    03/27/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncapRequest.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncapRequest.java
@@ -1,10 +1,62 @@
 package com.accenture.spm.server.serversocket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerEncapRequest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerEncapRequest server = new ServerEncapRequest();
+		server.start();
+	}
+	
+	private ServerSocket serverSocket;
+	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("ServerSocket Failure....");
+		}
+	}
+	
+	public void receive() {
+		try {
+			Socket client = serverSocket.accept();
+			System.out.println("1 client Connected....");
+			//获取请求协议
+			InputStream is =client.getInputStream();
+			byte[] datas = new byte[1024*10];
+			int len = is.read(datas);
+			String requestInfo = new String(datas,0,len);
+			System.out.println(requestInfo);
+			
+			Response response =new Response(client);
+			//关注了内容
+			response.print("<html>");
+			response.print("<head>");
+			response.print("<title>");
+			response.print("服务器响应成功");
+			response.print("</title>");
+			response.print("</head>");
+			response.print("<body>");
+			response.print("shsxt server终于回来了。。。。");
+			response.print("</body>");
+			response.print("</html>");
+			//关注了状态码
+			response.pushToBrowser(200);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Client Error!");
+		}
+	}
+	
+	public void stop() {
+		
 	}
 
 }
