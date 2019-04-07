2ad037721ce59774e116a2f6462e6af544e66f64
specter01wj <specter01wj@gmail.com>
2019-04-07 19:14:03 -0400

    03/27/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serversocket/ServerEncapResponseMap.java
@@ -0,0 +1,10 @@
+package com.accenture.spm.server.serversocket;
+
+public class ServerEncapResponseMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
