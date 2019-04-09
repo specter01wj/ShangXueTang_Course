85b4ed890c4b801f4b6d5e001f9bb271131531fb
specter01wj <specter01wj@gmail.com>
2019-04-09 17:33:43 -0400

    04/09/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/ServerEncapConfig.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/ServerEncapConfig.java
@@ -1,10 +1,58 @@
 package com.accenture.spm.server.serverServlet;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerEncapConfig {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerEncapConfig server = new ServerEncapConfig();
+		server.start();
+	}
+	
+	private ServerSocket serverSocket;
+	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("ServerSocket Failure....");
+		}
+	}
+	
+	public void receive() {
+		try {
+			Socket client = serverSocket.accept();
+			System.out.println("1 client Connected....");
+			//获取请求协议
+			Request request = new Request(client);
+			Response response = new Response(client);
+			
+			//关注了内容
+			Servlet servlet= null;
+			if(request.getUrl().equals("login")) {
+				servlet= new LoginServlet();
+			}else if(request.getUrl().equals("reg")) {
+				servlet= new RegisterServlet();
+			}else {
+				//首页....
+				servlet= new LoginServlet();
+			}
+			
+			servlet.service(request, response);		
+			//关注了状态码
+			response.pushToBrowser(200);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Client Error!");
+		}
+	}
+	
+	public void stop() {
+		
 	}
 
 }
