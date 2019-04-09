3b695eaf40508f94ef01055052d25156c2a93343
specter01wj <specter01wj@gmail.com>
2019-04-09 17:30:53 -0400

    04/09/2019 v1

---
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/Entity.java
@@ -0,0 +1,28 @@
+package com.accenture.spm.server.serverServlet;
+
+public class Entity {
+
+	private String name;
+	private String clz;
+	
+	public Entity() {
+		super();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getClz() {
+		return clz;
+	}
+	
+	public void setClz(String clz) {
+		this.clz = clz;
+	}
+	
+}
