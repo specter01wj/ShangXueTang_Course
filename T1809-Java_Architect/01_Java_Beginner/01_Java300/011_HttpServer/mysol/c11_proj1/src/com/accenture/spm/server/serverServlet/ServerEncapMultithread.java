dde5d59944c3ae165fda13b479f1ed4ae69c06ad
specter01wj <specter01wj@gmail.com>
2019-04-11 01:04:47 -0400

    04/10/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/ServerEncapMultithread.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/ServerEncapMultithread.java
@@ -1,10 +1,56 @@
 package com.accenture.spm.server.serverServlet;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerEncapMultithread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerEncapMultithread server = new ServerEncapMultithread();
+		server.start();
+	}
+	
+	private ServerSocket serverSocket;
+	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("ServerSocket Failure....");
+		}
+	}
+	
+	public void receive() {
+		try {
+			Socket client = serverSocket.accept();
+			System.out.println("1 client Connected....");
+			//获取请求协议
+			Request request = new Request(client);
+			Response response = new Response(client);
+			
+			//关注了内容
+			Servlet servlet= WebApp.getServletFromUrl(request.getUrl());
+			
+			if(null!=servlet) {
+				servlet.service(request, response);
+				//关注了状态码
+				response.pushToBrowser(200);
+			}else {
+				//错误....
+				response.pushToBrowser(404);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Client Error!");
+		}
+	}
+	
+	public void stop() {
+		
 	}
 
 }
