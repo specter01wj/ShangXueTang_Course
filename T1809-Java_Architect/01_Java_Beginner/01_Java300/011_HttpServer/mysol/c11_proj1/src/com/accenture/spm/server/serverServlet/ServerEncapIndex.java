d279f0ed085cc395473f3adb87b0b64a6b9a202d
specter01wj <specter01wj@gmail.com>
2019-04-11 01:36:41 -0400

    04/10/2019 v1

--- T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/ServerEncapIndex.java
+++ T1809-Java_Architect/01_Java_Beginner/01_Java300/011_HttpServer/mysol/c11_proj1/src/com/accenture/spm/server/serverServlet/ServerEncapIndex.java
@@ -1,10 +1,52 @@
 package com.accenture.spm.server.serverServlet;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerEncapIndex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerEncapIndex server = new ServerEncapIndex();
+		server.start();
 	}
 
+	private ServerSocket serverSocket;
+	private boolean isRunning;
+	
+	public void start() {
+		try {
+			serverSocket =  new ServerSocket(8888);
+			isRunning = true;
+			receive();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("ServerSocket Failure....");
+		}
+	}
+	
+	public void receive() {
+		while(isRunning) {
+			try {
+				Socket client = serverSocket.accept();
+				System.out.println("1 client Connected....");
+				//多线程处理
+				new Thread(new Dispatcher(client)).start();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Client Error!");
+			}
+		}
+	}
+	
+	public void stop() {
+		isRunning = false;
+		try {
+			this.serverSocket.close();
+			System.out.println("Server Stopped!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
